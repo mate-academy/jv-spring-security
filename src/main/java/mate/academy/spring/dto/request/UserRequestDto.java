5085c9abfc4c36e260072ed5dea8361b412ce082
divertitoS <astanislav2361@gmail.com>
2021-06-19 16:04:44 +0300

    Implemented EmailValidator, PasswordValidator.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,6 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
+
+@Password
 public class UserRequestDto {
+    @Email
     private String email;
     private String password;
 
---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final Pattern VALID_EMAIL_REGEX
+            = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)"
+            + "*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$");
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email != null) {
+            Matcher matcher = VALID_EMAIL_REGEX.matcher(email);
+            return matcher.matches();
+        }
+        return false;
+    }
+}
