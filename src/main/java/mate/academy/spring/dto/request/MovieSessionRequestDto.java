b177479ca83dd899581989ea205b134a0dce1bfc
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-06-26 21:41:05 +0300

    Added security layer

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,8 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 public class MovieSessionRequestDto {
+    @Positive
     private Long movieId;
+    @Positive
     private Long cinemaHallId;
+    @NotBlank
     private String showTime;
 
     public Long getMovieId() {
---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,56 @@
+package mate.academy.spring.validation;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])"
+            + "(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,100}$");
+
+    @Override
+    public boolean isValid(UserRequestDto user,
+                           ConstraintValidatorContext context) {
+        context.disableDefaultConstraintViolation();
+        return !isPasswordNull(user, context)
+                && isPasswordEquals(user, context)
+                && isPasswordMatchesPattern(user, context);
+    }
+
+    private boolean isPasswordMatchesPattern(UserRequestDto user,
+                                             ConstraintValidatorContext context) {
+        boolean valid = true;
+        if (!EMAIL_PATTERN.matcher(user.getPassword()).matches()) {
+            valid = false;
+            context.buildConstraintViolationWithTemplate("Password must contain "
+                    + "minimum 8 and maximum 100 characters, "
+                    + "at least one uppercase letter, "
+                    + "one lowercase letter, one number and one special character @$!%*?&")
+                    .addConstraintViolation();
+        }
+        return valid;
+    }
+
+    private boolean isPasswordEquals(UserRequestDto user,
+                                     ConstraintValidatorContext context) {
+        boolean valid = true;
+        if (!user.getPassword().equals(user.getRepeatPassword())) {
+            valid = false;
+            context.buildConstraintViolationWithTemplate("Both passwords must be the same")
+                    .addConstraintViolation();
+        }
+        return valid;
+    }
+
+    private boolean isPasswordNull(UserRequestDto user,
+                                     ConstraintValidatorContext context) {
+        boolean valid = false;
+        if (user.getPassword() == null || user.getRepeatPassword() == null) {
+            valid = true;
+            context.buildConstraintViolationWithTemplate("Password can't be null")
+                    .addConstraintViolation();
+        }
+        return valid;
+    }
+}
