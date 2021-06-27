a1c7d551753be98d5a946587009522dbd7788dfe
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-06-27 23:51:30 +0300

    created SecurityConfig, WebSecurity Initializer and added dependency to pom.xml

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @NotNull
     private String email;
+    @Size(min = 6)
     private String password;
 
     public String getEmail() {
