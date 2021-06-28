3b5cf2aadf6797243dae9d3dcf3c8113a45abc3a
Maksim <max.freedman.main@gmail.com>
2021-06-28 20:04:06 +0300

    Added email validator

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,14 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    public static final String EMAIL_PATTERN =
+            "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return email.matches(EMAIL_PATTERN);
+    }
+}
