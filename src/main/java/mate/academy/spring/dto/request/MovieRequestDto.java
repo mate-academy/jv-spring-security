c939dfdd3643761738a934eba4eb24b4b4038b00
IhorRome <ihor.rome@gmail.com>
2021-06-29 18:06:36 +0300

    basic solution

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull
+    @Size(max = 30)
     private String movieTitle;
+    @NotNull
+    @Size(max = 50)
     private String movieDescription;
 
     public String getMovieTitle() {
---
+++ src/main/java/mate/academy/spring/validation/impl/EmailValidator.java
@@ -0,0 +1,14 @@
+package mate.academy.spring.validation.impl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.validation.Email;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return email != null && email.matches(EMAIL_PATTERN);
+    }
+}
