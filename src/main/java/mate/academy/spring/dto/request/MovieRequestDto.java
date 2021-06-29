32f6809e95a07478075018c183a61dd5173b32cc
VladislavTetar <luxeon010203@gmail.com>
2021-06-29 16:40:12 +0300

    fixed travis.

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -2,10 +2,8 @@ package mate.academy.spring.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public class MovieRequestDto {
-    private static final int MIN_MOVIE_TITLE_LENGTH = 2;
     @NotBlank
     private String movieTitle;
     @NotNull
