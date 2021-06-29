4839fae405a0c770bb1603ec119beb9f9a3b1e65
Дмитро <dima4994@gmail.com>
2021-06-29 13:27:35 +0300

    fixed all mistakes notice by Roman Dubovskyi!

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,10 +1,11 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class CinemaHallRequestDto {
-    @NotNull
+    @Min(1)
     private int capacity;
     @NotNull
     @Max(256)
