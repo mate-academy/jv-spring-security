1ed467a540c91471da1de2b2a607479f483e26e9
Alina <vasylyshyn.alina@gmail.com>
2021-06-29 16:02:00 +0300

    first solution

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,11 +1,13 @@
 package mate.academy.spring.controller;
 
+import java.util.Optional;
 import mate.academy.spring.dto.response.ShoppingCartResponseDto;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.ShoppingCartMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +33,15 @@ public class ShoppingCartController {
     }
 
     @PostMapping("/movie-sessions")
-    public void addToCart(@RequestParam Long userId, @RequestParam Long movieSessionId) {
+    public void addToCart(Authentication authentication, @RequestParam Long movieSessionId) {
+        Optional<User> userOptional = userService.findByEmail(authentication.getName());
         shoppingCartService.addSession(
-                movieSessionService.get(movieSessionId), userService.get(userId));
+                movieSessionService.get(movieSessionId), userOptional.get());
     }
 
     @GetMapping("/by-user")
-    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
-        User user = userService.get(userId);
-        return shoppingCartMapper.mapToDto(shoppingCartService.getByUser(user));
+    public ShoppingCartResponseDto getByUser(Authentication authentication) {
+        Optional<User> userOptional = userService.findByEmail(authentication.getName());
+        return shoppingCartMapper.mapToDto(shoppingCartService.getByUser(userOptional.get()));
     }
 }
