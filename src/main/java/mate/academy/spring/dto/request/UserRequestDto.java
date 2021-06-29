7fd3ad10dbca3beb6d15ae6bc1ac24769823874a
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-06-29 15:40:12 +0300

    completed task

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,14 +1,46 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.FieldsValueMatch;
+
+@FieldsValueMatch(
+        field = "password",
+        fieldMatch = "repeatPassword",
+        message = "Passwords don't match!"
+)
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 8)
     private String password;
+    @NotNull
+    @Size(min = 8)
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
