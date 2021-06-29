3e1210c284c2e2a377804fe889a60d1805817b15
SerKisJD <sergej.kislyakov@gmail.com>
2021-06-29 14:10:12 +0300

    fixed all

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -3,7 +3,9 @@ package mate.academy.spring.dto.request;
 import javax.validation.constraints.NotNull;
 
 public class MovieSessionRequestDto {
+    @NotNull
     private Long movieId;
+    @NotNull
     private Long cinemaHallId;
     @NotNull
     private String showTime;
