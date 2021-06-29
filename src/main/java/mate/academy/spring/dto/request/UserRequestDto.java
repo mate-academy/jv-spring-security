fcecb3b9c151d3f969af68b6492d9fbb7a999573
Ihor-Nykyforov <kifa1203@gmail.com>
2021-06-29 13:31:09 +0300

    First solution

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @Min(value = 4)
     private String password;
 
     public String getEmail() {
