91f6d17b25556ab041e3743b7e50469825203f6d
ilia <bulkaus@gmail.com>
2021-06-29 20:00:28 +0300

    fix edits

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -2,19 +2,13 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class MovieRequestDto {
     @NotNull
     private String movieTitle;
-    private String movieDescription;
-
     @NotNull
     @Size(min = 15)
-    public String getMovieTitle() {
-        return movieTitle;
-    }
-
-    public String getMovieDescription() {
-        return movieDescription;
-    }
+    private String movieDescription;
 }
