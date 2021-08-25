eb95a6f95fdb52fa5df9a43c8043f1469fc16605
Benito <zara.benito17@gmail.com>
2021-08-25 22:44:45 +0300

    added solution but CustomGlobalExceptionHandler doesn't work(

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,8 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class MovieSessionRequestDto {
+    @NotNull
+    @Min(value = 0)
     private Long movieId;
+    @NotNull
+    @Min(value = 0)
     private Long cinemaHallId;
+    @NotNull
     private String showTime;
 
     public Long getMovieId() {
