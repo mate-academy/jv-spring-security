0bc3272f8cd0ed76489c8f0ff7e65d727e76dfe3
PashaTerletskyi <p.terletsky@gmail.com>
2021-08-25 20:16:18 +0300

    Added solution

---
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.config;
+
+import mate.academy.spring.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User user = new User();
+        user.setEmail("Bob@gmail.com");
+        user.setPassword("1234");
+        auth.inMemoryAuthentication()
+                .withUser(user.getEmail())
+                .password(getEncoder().encode(user.getPassword()))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
