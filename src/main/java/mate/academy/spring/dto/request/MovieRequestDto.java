16891e35b2128bba9ede023ff28504423edc7a1a
PashaTerletskyi <p.terletsky@gmail.com>
2021-08-25 21:43:19 +0300

    Added @Min(value = 1) to MovieRequestDto.java

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,11 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class MovieRequestDto {
     @NotNull
+    @Min(value = 1)
     private String movieTitle;
     @NotNull
+    @Min(value = 1)
     private String movieDescription;
 
     public String getMovieTitle() {
