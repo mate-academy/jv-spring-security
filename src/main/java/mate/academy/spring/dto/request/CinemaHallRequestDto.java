4bf25b29ee080cd332d33a69e83d60bbe419507e
olesiasmitiukh <d.olleska@gmail.com>
2021-08-25 18:45:27 +0300

    added first solution

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,6 +1,9 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+
 public class CinemaHallRequestDto {
+    @Min(value = 12)
     private int capacity;
     private String description;
 
--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,8 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+
 public class MovieSessionRequestDto {
     private Long movieId;
     private Long cinemaHallId;
+    @NotNull
     private String showTime;
 
     public Long getMovieId() {
