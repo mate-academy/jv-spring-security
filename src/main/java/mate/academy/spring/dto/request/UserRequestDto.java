7e28d910447137ffbe10b615e63f98d34daabc75
EugeneSinica <eugenesinica@gmail.com>
2021-08-26 16:54:48 +0300

    deleten @NotNull from UserRequestDto

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 public class UserRequestDto {
-    @NotNull
     @Email
     private String email;
     @NotNull
