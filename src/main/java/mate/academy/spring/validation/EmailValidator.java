141852f67048e7891c22a10bf6178058c3d4b8d8
Yura Lug <LUV97@ukr.net>
2021-08-26 15:47:18 +0300

    V 2.0

--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -1,15 +1,13 @@
 package mate.academy.spring.validation;
 
-import java.util.regex.Pattern;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^([\\w-\\.]+){1,64}@"
-            + "([\\w&&[^_]]+){2,255}.[a-z]{2,}$", Pattern.CASE_INSENSITIVE);
+    private static final String EMAIL_PATTERN = "^(.+)@(.+)$";
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        return email != null && EMAIL_PATTERN.matcher(email).matches();
+        return email != null && email.matches(EMAIL_PATTERN);
     }
 }
