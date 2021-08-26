1f3c231493be5ae8ef2ad2101bf10a1bc41b15a1
sSlonIk <kryachok@gmail.com>
2021-08-26 15:30:28 +0300

    created some scurity

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -1,11 +1,12 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.dto.response.ShoppingCartResponseDto;
-import mate.academy.spring.model.User;
+import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.ShoppingCartMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +32,16 @@ public class ShoppingCartController {
     }
 
     @PostMapping("/movie-sessions")
-    public void addToCart(@RequestParam Long userId, @RequestParam Long movieSessionId) {
-        shoppingCartService.addSession(
-                movieSessionService.get(movieSessionId), userService.get(userId));
+    public void addToCart(Authentication auth, @RequestParam Long movieSessionId) {
+        shoppingCartService.addSession(movieSessionService.get(movieSessionId),
+                userService.findByEmail(auth.getName()).orElseThrow(() ->
+                new DataProcessingException("User not found/exist. User: " + auth.getName())));
     }
 
     @GetMapping("/by-user")
-    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
-        User user = userService.get(userId);
-        return shoppingCartMapper.mapToDto(shoppingCartService.getByUser(user));
+    public ShoppingCartResponseDto getByUser(Authentication auth) {
+        return shoppingCartMapper.mapToDto(shoppingCartService.getByUser(userService
+                .findByEmail(auth.getName()).orElseThrow(() ->
+                new DataProcessingException("User not found/exist. User: " + auth.getName()))));
     }
 }
