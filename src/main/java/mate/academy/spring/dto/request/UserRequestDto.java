9eada608dbb84dc86195156a4f16aac5ad7c6711
vKutik <84785564+vKutik@users.noreply.github.com>
2021-08-26 20:17:59 +0300

    add javax library for validation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,6 +1,6 @@
 package mate.academy.spring.dto.request;
 
-import jakarta.validation.constraints.Size;
+import javax.validation.constraints.Size;
 import mate.academy.spring.validate.Email;
 
 public class UserRequestDto {
