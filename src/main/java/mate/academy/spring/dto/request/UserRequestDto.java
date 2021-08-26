ad42121e3ec3763004616a1bccc7e08de2d4a917
Nikitozin <nikitozin91@gmail.com>
2021-08-26 08:52:12 +0300

    CheckStyle

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,8 @@
 package mate.academy.spring.dto.request;
 
-import mate.academy.spring.validation.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
     @NotNull
