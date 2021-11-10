0b4658b4526df92a0d585e99ad5e9dba12b2a959
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-11-10 15:27:44 +0200

    CustomGlobalExceptionHandler created

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,20 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import mate.academy.spring.validation.ConfirmPassword;
+import mate.academy.spring.validation.Email;
+
+@ConfirmPassword
 public class UserRequestDto {
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
+    @Min(value = 5)
     private String password;
+    @NotBlank
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +22,13 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
