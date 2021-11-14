770c44ff6c6adfc1e48215514d898339622b4141
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-11-14 10:54:09 +0200

    Travis

--- src/main/java/mate/academy/spring/validation/FieldsValueMatchValidator.java
+++ src/main/java/mate/academy/spring/validation/FieldsValueMatchValidator.java
@@ -1,8 +1,8 @@
 package mate.academy.spring.validation;
 
-import org.springframework.beans.BeanWrapperImpl;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class FieldsValueMatchValidator
         implements ConstraintValidator<FieldsValueMatch, Object> {
