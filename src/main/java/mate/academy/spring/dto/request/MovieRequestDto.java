b4af4f7e79b982971555bcab121ac2ca6a7d65f1
Vladyslav Gural <vladgural@gmail.com>
2021-11-14 22:47:28 +0200

    added GlobalExceptionHandler

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,9 +1,11 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class MovieRequestDto {
     @NotNull
+    @Size(min = 10)
     private String movieTitle;
     @NotNull
     private String movieDescription;
