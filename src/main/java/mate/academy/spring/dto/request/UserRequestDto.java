29127a7d1d8c9b3a0ee333a26a97ffd36517b7d9
Vladyslav Gural <vladgural@gmail.com>
2021-11-14 21:14:09 +0200

    added EmailValidator

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,9 +2,10 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
-    @NotNull
+    @Email
     private String email;
     @NotNull
     @Size(min = 4)
