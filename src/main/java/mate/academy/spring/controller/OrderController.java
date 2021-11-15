cc72837c6d97c61814189b4ca6b982be9f7c8425
Yuliya Senchenko <usencenko70@gmail.com>
2021-11-15 14:21:15 +0200

    Spring security project

--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,10 +44,10 @@ public class OrderController {
 
     @GetMapping
     public List<OrderResponseDto> getOrderHistory(Authentication authentication) {
-            User user = userService.findByEmail(authentication.getName()).orElseThrow();
-            return orderService.getOrdersHistory(user)
-                    .stream()
-                    .map(orderMapper::mapToDto)
-                    .collect(Collectors.toList());
+        User user = userService.findByEmail(authentication.getName()).orElseThrow();
+        return orderService.getOrdersHistory(user)
+                .stream()
+                .map(orderMapper::mapToDto)
+                .collect(Collectors.toList());
     }
 }
