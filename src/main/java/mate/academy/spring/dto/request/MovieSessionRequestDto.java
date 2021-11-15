1e25762be6989e9b9270927e0039c48c41d44346
Andrii Tyndiuk <andriytundyk@gmail.com>
2021-11-15 19:17:17 +0200

    complete the task spring security

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class MovieSessionRequestDto {
+    @NotNull
+    @Positive
     private Long movieId;
+    @NotNull
+    @Positive
     private Long cinemaHallId;
     private String showTime;
 
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.EmailValidation;
+
 public class UserRequestDto {
+    @EmailValidation
     private String email;
+    @NotNull
+    @Size(min = 4,max = 21)
     private String password;
 
     public String getEmail() {
