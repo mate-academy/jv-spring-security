85f4252911e9f0ae808c65e06778e5db0bf1b20c
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-11-15 20:31:25 +0200

    Implemented spring security

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Min(value = 10)
     private int capacity;
+    @NotNull
+    @Size(min = 10, max = 300)
     private String description;
 
     public int getCapacity() {
