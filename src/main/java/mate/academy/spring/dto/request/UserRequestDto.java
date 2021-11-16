fe8582e313f10f92cfbc84af1f465d57e2741d04
Vitalii Mykh <vitaliymykh@gmail.com>
2021-11-16 17:29:11 +0200

    spring security added

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @NotNull
+    @Email
+    @Size(min = 4)
     private String email;
+    @NotNull
+    @Size(min = 4)
     private String password;
 
     public String getEmail() {
