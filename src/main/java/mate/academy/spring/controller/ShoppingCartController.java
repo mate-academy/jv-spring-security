043513044d4497437793e1ab7373214e1aa50088
Pasha Rakitin <dogovor@lan.com.ua>
2021-11-16 02:48:43 +0200

    spring-security

--- src/main/java/mate/academy/spring/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/spring/controller/ShoppingCartController.java
@@ -6,6 +6,7 @@ import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.ShoppingCartMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +32,15 @@ public class ShoppingCartController {
     }
 
     @PutMapping("/movie-sessions")
-    public void addToCart(@RequestParam Long userId, @RequestParam Long movieSessionId) {
+    public void addToCart(Authentication authentication, @RequestParam Long movieSessionId) {
         shoppingCartService.addSession(
-                movieSessionService.get(movieSessionId), userService.get(userId));
+                    movieSessionService.get(movieSessionId), userService.findByEmail(authentication
+                                .getName()).get());
     }
 
     @GetMapping("/by-user")
-    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
-        User user = userService.get(userId);
+    public ShoppingCartResponseDto getByUser(Authentication authentication) {
+        User user = userService.findByEmail(authentication.getName()).get();
         return shoppingCartMapper.mapToDto(shoppingCartService.getByUser(user));
     }
 }
