6ed4df1554e34078ff38d202e3492e13b2d1c98f
Stas Shepet <milka33349@gmail.com>
2021-11-16 02:32:33 +0200

    spring security was implemented

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,5 +1,7 @@
 package mate.academy.spring.controller;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import mate.academy.spring.dto.response.UserResponseDto;
 import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.User;
@@ -22,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping("/by-email")
-    public UserResponseDto getByEmail(@RequestParam String email) {
+    public UserResponseDto getByEmail(@RequestParam @NotNull @Email String email) {
         User user = userService.findByEmail(email).orElseThrow(
                 () -> new DataProcessingException("Invalid email"));
         return userMapper.mapToDto(user);
---
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -0,0 +1,21 @@
+package mate.academy.spring.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
