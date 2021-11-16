1a77ac1bd848f26adad258de76187218a1b25646
Nazar Hetun <nazarhetun@gmail.com>
2021-11-16 17:21:20 +0200

    implement spring security

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 public class CinemaHallRequestDto {
+    @Min(1)
     private int capacity;
+    @NotNull
+    @Length(min = 10, max = 255)
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 public class MovieRequestDto {
+    @NotNull
     private String movieTitle;
+    @NotNull
+    @Length(min = 10, max = 255)
     private String movieDescription;
 
     public String getMovieTitle() {
