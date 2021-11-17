9dcecb960b36c0f3bf14d27f1e668704c3085162
pustoj0 <pustoj0@gmail.com>
2021-11-17 14:44:32 +0200

    configure spring security.

---
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder builder)
+            throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("email1@gmail.com")
+                .password("qwerty")
+                .roles("USER");
+    }
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
