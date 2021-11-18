92e7c58f48212aa6b69af88e5bb7d28e71d1eb0c
Gleb Kreshchuk <gleb.net@gmail.com>
2021-11-19 00:18:37 +0200

    SecurityConfig slightly changed

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("root@m.com")
+                .withUser("root")
                 .password(getEncoder().encode("1111"))
                 .roles("USER");
     }
