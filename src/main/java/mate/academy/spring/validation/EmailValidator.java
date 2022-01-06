7b661936980f891e1fda2cc9367cfc6dfc6dd10e
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2022-01-06 17:20:40 +0200

    hw is completed

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,14 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    public static final String VALID_EMAIL_PATTERN
+            = "^(\\w+\\.?)+\\w+@([\\w]+\\.?)+\\w{2,}$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email.matches(VALID_EMAIL_PATTERN);
+    }
+}
