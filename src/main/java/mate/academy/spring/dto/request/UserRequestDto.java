d46baa004f632b4b05b92a13a20bfbc160cd6093
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-13 00:05:28 +0200

    implemented task 'spring-security'

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,14 +1,26 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.Getter;
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.ValidPassword;
+
+@Getter
+@ValidPassword(message = "Passwords must be the same")
 public class UserRequestDto {
+    @Email
+    @NotNull(message = "Email may not be empty")
     private String email;
+    @NotNull(message = "Password may not be empty")
+    @Pattern(regexp =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}\\[\\]:;<>,?/~_+-=|]).{8,32}$",
+            message = "Password must contain:\n"
+                    + "a minimum of 1 numeric character (0-9) and\n"
+                    + "a minimum of 1 upper case letter (A-Z) and\n"
+                    + "a minimum of 1 lower case letter (a-z) and\n"
+                    + "a minimum of 1 special character: ~`!@#$%^&*()-_+={}[]|\\;:\"<>,./?\n"
+                    + "password is 8-16 characters with no space")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    private String repeatPassword;
 }
