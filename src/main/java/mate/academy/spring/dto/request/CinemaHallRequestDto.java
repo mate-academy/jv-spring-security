855c12510001ad3ea79107d862325623ea190407
Zachar Chorny <ufumoveldar@gmail.com>
2022-01-14 20:05:08 +0200

    edits made

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class CinemaHallRequestDto {
-    @Min(value = 0)
+    @Min(value = 1)
     private int capacity;
     @NotNull
     private String description;
