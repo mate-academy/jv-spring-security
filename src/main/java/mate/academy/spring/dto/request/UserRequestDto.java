6ea655eb1f24d052aa8e0e3ab5c60a288f7dae92
Yaroslav Kavun <yka40znp@gmail.com>
2022-01-14 12:37:57 +0200

    implemented email validation using GlobalExceptionHandler

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,23 @@
 package mate.academy.spring.dto.request;
 
+import com.sun.istack.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validator.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 6, max = 18)
     private String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getEmail() {
         return email;
