b108a5d2872254799177a6001f19c988f7b492fc
VitaliiMarchenko <vitalbka.01@gmail.com>
2022-01-16 17:15:45 +0200

    initial commit

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
+    @NotNull
+    @Size(min = 3)
     private String email;
+    @NotNull
+    @Size(min = 3)
     private String password;
 
     public String getEmail() {
