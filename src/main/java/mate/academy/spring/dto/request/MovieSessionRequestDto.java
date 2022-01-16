10fa9f32e899bd3dcc4bf697e3c9c0b6e19008f6
Panchenko Volodymyr <volodymyr.panchenko147@egmail.com>
2022-01-16 21:49:27 +0200

    Spring Security was implemented

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,8 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class MovieSessionRequestDto {
+    @Min(value = 1)
     private Long movieId;
+    @Min(value = 1)
     private Long cinemaHallId;
+    @NotNull
     private String showTime;
 
     public Long getMovieId() {
