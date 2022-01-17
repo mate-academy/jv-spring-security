360f6d199c881ee288331bf1c9540a38af2000ae
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2022-01-17 14:01:51 +0200

    Added solution according to README

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import com.sun.istack.NotNull;
+import mate.academy.spring.validator.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
     private String password;
 
     public String getEmail() {
