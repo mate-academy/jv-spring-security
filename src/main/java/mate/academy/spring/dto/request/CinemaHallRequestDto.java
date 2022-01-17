aab5e73f51ff67e0790f0e2450be8d120758a3e9
Vladyslav Chyver <lione2009lione@gmail.com>
2022-01-17 17:17:12 +0200

    code was optimized after review

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class CinemaHallRequestDto {
-    @NotNull
     @Min(value = 1)
     private int capacity;
     @NotNull
