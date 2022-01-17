ab0e9a9c0089e4295f0b9bbb4efc0e9fbcf6e1f0
Zachar Chorny <ufumoveldar@gmail.com>
2022-01-17 11:36:23 +0200

    edits made(2)

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,8 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import mate.academy.spring.validation.Email;
+
 
 public class UserRequestDto {
     @NotNull
