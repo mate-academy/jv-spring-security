7d1268ac0d9868a4b237c95d743eb01b33c37720
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2022-01-17 15:39:27 +0200

    Small fix

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -2,12 +2,10 @@ package mate.academy.spring.dto.request;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public class CinemaHallRequestDto {
     @Max(150)
     @Min(10)
-    @NotNull
     private int capacity;
     private String description;
 
