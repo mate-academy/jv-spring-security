a30bee7c96fe1a3400a14941613f7d8de15b1daa
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2022-01-17 16:56:34 +0200

    mistake was fixed

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,7 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import mate.academy.spring.service.validation.Email;
 
 public class UserRequestDto {
     @Email
