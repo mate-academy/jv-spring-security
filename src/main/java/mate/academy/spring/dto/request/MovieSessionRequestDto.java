fc25bfbc7079ddbf53ad3583807506dc3775a9d5
Panchenko Volodymyr <volodymyr.panchenko147@egmail.com>
2022-01-17 13:37:44 +0200

    Fixes were implemented

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotNull;
 
 public class MovieSessionRequestDto {
     @Min(value = 1)
+    @NotNull
     private Long movieId;
     @Min(value = 1)
+    @NotNull
     private Long cinemaHallId;
     @NotNull
     private String showTime;
