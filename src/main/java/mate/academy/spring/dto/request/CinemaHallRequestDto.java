25a656b4c1267707e8cf78211aafbefa7dbd68bf
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2022-01-17 15:23:04 +0200

    Project created

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,6 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public class CinemaHallRequestDto {
+    @Min(value = 1)
+    @Max(value = 1000)
     private int capacity;
     private String description;
 
---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,15 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "/^((([0-9A-Za-z]{1}[-0-9A-z\\.]{1,}"
+            + "[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\\.]{1,}[0-9А-Яа-я]{1}))"
+            + "@([-A-Za-z]{1,}\\.){1,2}[-A-Za-z]{2,})$/u";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return (email == null) && (email.matches(EMAIL_PATTERN));
+    }
+}
