5900aa9291d034cd2982a82b1067d6748c9b012b
Dzmitry Kozel <dmitrokoz88@gmail.com>
2022-01-17 14:10:19 +0200

    BUILD SUCCESS;

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,14 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class MovieRequestDto {
+    @NotNull
+    @Size(min = 1)
     private String movieTitle;
+    @NotNull
     private String movieDescription;
-
-    public String getMovieTitle() {
-        return movieTitle;
-    }
-
-    public String getMovieDescription() {
-        return movieDescription;
-    }
 }
