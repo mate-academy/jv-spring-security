087ba56e6ac2fa46e3b9b46ecebc8f3b1d2aff6f
Oleh Mazurkov <o.mazurkov@gmail.com>
2022-03-16 02:14:53 +0200

    solution jv-spring-security

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -5,7 +5,7 @@ import mate.academy.spring.dto.request.UserRequestDto;
 import mate.academy.spring.dto.response.UserResponseDto;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
-import mate.academy.spring.service.mapper.UserMapper;
+import mate.academy.spring.service.mapper.ResponseDtoMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authService;
-    private final UserMapper userMapper;
+    private final ResponseDtoMapper<UserResponseDto, User> userMapper;
 
-    public AuthenticationController(AuthenticationService authService, UserMapper userMapper) {
+    public AuthenticationController(AuthenticationService authService,
+                                    ResponseDtoMapper<UserResponseDto, User> userMapper) {
         this.authService = authService;
         this.userMapper = userMapper;
     }
