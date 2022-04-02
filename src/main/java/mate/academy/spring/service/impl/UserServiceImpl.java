809e76984debf01f0415623221d554721078dcd9
Sasha Bachynskyi <allfred12@gmail.com>
2022-04-02 12:33:33 +0300

    removed method login in AuthenticationService, PasswordUtil and salt from User entity, modified method add in UserService with PasswordEncoder

--- src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.util.PasswordUtil;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +19,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User add(User user) {
-        String salt = PasswordUtil.getSalt(SALT_LENGTH);
-        String securePassword = PasswordUtil.generateSecurePassword(user.getPassword(), salt);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String securePassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(securePassword);
-        user.setSalt(salt);
         return userDao.add(user);
     }
 
