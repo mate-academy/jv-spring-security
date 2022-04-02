6be882428cda0fc56ec6284f1a1d4c9cb3103fff
Sasha Bachynskyi <allfred12@gmail.com>
2022-04-02 11:16:18 +0300

    replaced all ?userId with authentication object

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("flash")
+                .withUser("flash@i.ua")
                 .password(getEncoder().encode("1234"))
                 .roles("USER");
     }
