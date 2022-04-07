a8fbf3045be62fa23b34e3e8d2f393fb85bafd44
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-07 18:06:19 +0300

    added customized email annotation

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.validation;
+
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
