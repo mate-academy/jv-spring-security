2e86f1453fcae7db920baa44e221f4d20e2eb04c
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-07 19:23:56 +0300

    added valid annotations and CustomGlobalExceptionHandler

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,10 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
     @Email
+    @NotNull
     private String email;
+    @NotNull
+    @Size(min = 4, max = 24)
     private String password;
 
     public String getEmail() {
