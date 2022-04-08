6d37f1f7ff9df0d07ef0036686dc2f0e3d33e90b
Andriy_Poluhovic <chimbar@ukr.net>
2022-04-08 12:42:26 +0300

    corrected mistakes

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Password;
@@ -11,11 +10,9 @@ public class UserRequestDto {
     @Email
     @Size(min = 6, max = 100)
     private String email;
-    @NotEmpty
     @NotNull
     @Size(min = 8, max = 30)
     private String password;
-    @NotEmpty
     @NotNull
     @Size(min = 8, max = 30)
     private String repeatPassword;
