f92b3fe1d29d09a2889839b7bcfa3e2e1c36e2b5
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-11 22:51:01 +0300

    Add annotations from hibernate-validator

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,10 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotBlank;
+
 public class UserRequestDto {
     private String email;
+    @NotBlank
     private String password;
 
     public String getEmail() {
