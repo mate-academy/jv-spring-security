089f3a5112efd4a2488c90f01b32b84b8a4604d3
sviat-h <svyatoslav.grinik@gmail.com>
2022-04-12 14:41:45 +0300

    code was refactored according to the comments

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,11 +1,12 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 public class CinemaHallRequestDto {
     @Min(20)
     private int capacity;
-    @Min(50)
+    @Size(min = 50)
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,12 +1,12 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class MovieRequestDto {
     @NotNull
     private String title;
-    @Min(50)
+    @Size(min = 50)
     private String description;
 
     public String getTitle() {
