3b6a48deb4a7cee48bcaf2ccee27f6c8f3335c31
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-13 14:18:12 +0300

    Change name of constant in EmailValidator

--- src/main/java/mate/academy/spring/lib/EmailValidator.java
+++ src/main/java/mate/academy/spring/lib/EmailValidator.java
@@ -4,10 +4,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String REGEX = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
+    private static final String EMAIL_PATTERN = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value != null && value.matches(REGEX);
+        return value != null && value.matches(EMAIL_PATTERN);
     }
 }
