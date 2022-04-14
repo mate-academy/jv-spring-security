89aa3dac9b3c6a26b1cb33a673e722c897a91f3e
Vadym <vadya829@gmail.com>
2022-04-14 11:52:54 +0200

    checkstyle

--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -7,6 +7,7 @@ import mate.academy.spring.dto.request.UserRequestDto;
 public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
     @Override
     public boolean isValid(UserRequestDto userRequestDto, ConstraintValidatorContext context) {
-        return userRequestDto != null && userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
+        return userRequestDto != null
+                && userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
     }
 }
