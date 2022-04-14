2f5beb9905611f5ee69376ab1d703a2845f8890b
nastyaprokhorenko <nastyaprokhorenko527@gmail.com>
2022-04-14 21:15:05 +0300

    fixed checkstyle

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -7,7 +7,6 @@ import mate.academy.spring.service.mapper.ResponseDtoMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
