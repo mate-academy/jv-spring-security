7ae18393b3a1a97badc6be57ead05ebf0dcbc44f
anyutamysko <a_misko@ukr.net>
2022-04-20 21:53:36 +0300

    spring security part 1

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import mate.academy.spring.util.DateTimePatternUtil;
 
 public class MovieSessionRequestDto {
+    @Min(value = 1)
     private Long movieId;
+    @Min(value = 1)
     private Long cinemaHallId;
+    @Future
+    @JsonFormat(pattern = DateTimePatternUtil.DATE_TIME_PATTERN)
     private LocalDateTime showTime;
 
     public Long getMovieId() {
