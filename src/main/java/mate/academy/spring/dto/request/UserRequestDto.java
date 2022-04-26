991e5026dda00ac1366b15c0a8bd48fb2c24f652
AprioriKS <sounddummies@gmail.com>
2022-04-26 18:10:35 +0300

    solved

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,14 +1,28 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import mate.academy.spring.validation.ValidPassword;
+
+@ValidPassword
 public class UserRequestDto {
+    @Email
     private String email;
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
