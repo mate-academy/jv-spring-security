299b7593b287afc19bfe74512afedbbe4f1ae7f0
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-29 16:02:10 +0300

    Fixed after student review

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,14 +1,15 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
     @Email
     private String email;
     @NotNull
-    @Min(4)
+    @Size(min = 4)
     private String password;
 
     public String getEmail() {
