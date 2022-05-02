77f8d44037e48bc109d99cd196b002f896252a51
IgorZima90 <zimyshkaa@gmail.com>
2022-05-03 02:31:06 +0300

    was added spring security and validation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @Size(min = 5, max = 15)
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -11,4 +17,13 @@ public class UserRequestDto {
     public String getPassword() {
         return password;
     }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
 }
+
