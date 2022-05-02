8b65189cd1d9ce8232d66e239aa526803a0730eb
Oleh Mytiai <mytiai.ov@icloud.com>
2022-05-02 22:57:44 +0300

    configured Spring Security, replaced all {userId} with Authenticatation object, added validation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class UserRequestDto {
+    @NotNull
     private String email;
+    @NotNull
+    @Min(6)
     private String password;
 
     public String getEmail() {
