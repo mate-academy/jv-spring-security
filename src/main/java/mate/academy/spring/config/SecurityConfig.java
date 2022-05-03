9502ef2a1bdadbc7aa75e497189fc0ccdedf40e7
Anton Ignatenko <87337009+antonignatenko@users.noreply.github.com>
2022-05-03 13:48:58 +0300

    changed inmemory authentication method, so it allows to open login page

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package mate.academy.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,27 +11,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("USER")
-                .password(getEncoder().encode("password"))
-                .roles("USER").and()
-                .withUser("USER").password("password").roles("USER", "ADMIN");;
+                .withUser("Bob").password(getEncoder().encode("test"))
+                .roles("USER")
+                .and()
+                .withUser("John").password(getEncoder().encode("test2"))
+                .roles("USER");
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+                .and().httpBasic();
     }
 
     @Bean
