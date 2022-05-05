24940efef0d25d96040d493bd1462ee9b8fce97f
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-05-05 22:45:52 +0300

    Spring Security

--- src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -4,24 +4,22 @@ import java.util.Optional;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.util.PasswordUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private static final int SALT_LENGTH = 10;
     private final UserDao userDao;
+    private final PasswordEncoder encoder;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder encoder) {
         this.userDao = userDao;
+        this.encoder = encoder;
     }
 
     @Override
     public User add(User user) {
-        String salt = PasswordUtil.getSalt(SALT_LENGTH);
-        String securePassword = PasswordUtil.generateSecurePassword(user.getPassword(), salt);
-        user.setPassword(securePassword);
-        user.setSalt(salt);
+        user.setPassword(encoder.encode(user.getPassword()));
         return userDao.add(user);
     }
 
