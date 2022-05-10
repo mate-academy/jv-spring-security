f167d03ed6234915d993d74021f9fb6e8fb6ba29
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-05-10 16:25:55 +0300

    Implemented spring security, exception handler and custom valid annotation.

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,9 +1,12 @@
 package mate.academy.spring.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotEmpty;
 
 public class MovieSessionRequestDto {
+    @NotEmpty
     private Long movieId;
+    @NotEmpty
     private Long cinemaHallId;
     private LocalDateTime showTime;
 
