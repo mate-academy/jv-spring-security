c1fd87945c93c78169b4211e4365792bac6da9de
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-05-20 15:56:55 +0200

    - added all for solution

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,20 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import org.hibernate.validator.constraints.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @Min(value = 8)
+    @Max(value = 64)
     private String password;
+    private String repeatPassword;
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
 
     public String getEmail() {
         return email;
@@ -10,5 +22,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
