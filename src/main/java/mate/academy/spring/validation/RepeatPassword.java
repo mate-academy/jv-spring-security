ad083c43b4c067af8bac86f0f5cbbb1174c06739
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-05-23 10:02:07 +0200

    - added corrections

--- src/main/java/mate/academy/spring/validation/RepeatPassword.java
+++ src/main/java/mate/academy/spring/validation/RepeatPassword.java
@@ -1,9 +1,7 @@
 package mate.academy.spring.validation;
 
-import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Constraint(validatedBy = RepeatPasswordValidator.class)
 public @interface RepeatPassword {
     String message() default "Incorrect password";
 
