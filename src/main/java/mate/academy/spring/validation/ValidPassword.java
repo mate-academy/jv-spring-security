13231b2b16351572eeb3ff97bbb983ff3fab737b
Maksym Pokras <mxpokras@gmail.com>
2022-05-28 10:43:20 +0200

    message text modified

--- src/main/java/mate/academy/spring/validation/ValidPassword.java
+++ src/main/java/mate/academy/spring/validation/ValidPassword.java
@@ -11,7 +11,7 @@ import javax.validation.Payload;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
-    String message() default "invalid email";
+    String message() default "invalid password";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
