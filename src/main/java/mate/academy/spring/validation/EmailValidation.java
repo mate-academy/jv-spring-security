6ab95d122cada1f156f76ee52c07e884de9b752f
Slava Cheban <37748459+annaRav@users.noreply.github.com>
2022-06-09 22:04:57 +0300

    Update src/main/java/mate/academy/spring/validation/EmailValidation.java
    Co-authored-by: Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>

--- src/main/java/mate/academy/spring/validation/EmailValidation.java
+++ src/main/java/mate/academy/spring/validation/EmailValidation.java
@@ -10,11 +10,6 @@ public class EmailValidation implements ConstraintValidator<Email, String> {
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        if (email == null) {
-            return false;
-        }
-        Pattern pattern = Pattern.compile(EMAIL_PATTERT);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        return email != null && email.matches(EMAIL_PATTERN);
     }
 }
