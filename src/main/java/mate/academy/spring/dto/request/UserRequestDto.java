93f0d4e65bdaae18d1d104d7c35bfc875bcadb1e
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2022-06-16 17:55:15 +0300

    Solution of the task

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @Email
+    @Size(min = 6,max = 50)
     private String email;
+    @Size(min = 2,max = 24)
     private String password;
 
     public String getEmail() {
