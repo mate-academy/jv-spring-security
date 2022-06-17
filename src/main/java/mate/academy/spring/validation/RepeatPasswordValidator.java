4d4b3359596d2e7c7db19d7214a1675488b08b60
oleg-new <9213195@gmail.com>
2022-06-17 22:43:09 +0300

    Set up Spring Security with in-memory authentication. created class CustomGlobalExceptionHandler Implemented validation of email and password repeat removed method `login()` in AuthenticationService

---
+++ src/main/java/mate/academy/spring/validation/RepeatPasswordValidator.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class RepeatPasswordValidator implements
+        ConstraintValidator<RepeatPassword, UserRequestDto> {
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        if (userRequestDto.getRepeatPassword() == null) {
+            return false;
+        }
+        return userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
+    }
+}
