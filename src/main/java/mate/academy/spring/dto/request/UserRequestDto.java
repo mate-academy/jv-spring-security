dfcff7efccea0f84858f8a83143a186329277dbb
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-06-20 13:44:11 +0200

    email's import was changed

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,7 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
     @Email
