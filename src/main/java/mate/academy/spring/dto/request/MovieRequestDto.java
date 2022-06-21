fa248827c16cec0ad735d80974854332f7f1b160
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-06-21 14:03:05 +0300

    Configured Spring Security with in memory authentication. Replaced all ?userId @RequestParam with Authentication object. Added annotations where applicable Implemented custom email validation. Implemented CustomGlobalExceptionHandler. Removed method login() in AuthenticationService, HashUtil and salt from User entity. Using passwordEncoder in UserService -> add() for password hashing.

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull
+    @Size(max = 255)
     private String title;
+    @Size(max = 255)
     private String description;
 
     public String getTitle() {
