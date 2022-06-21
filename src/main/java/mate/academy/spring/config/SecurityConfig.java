c76f5b4040e2a8a9811ae1e651f2317173e1eedf
Andrii Fabersky <Andriifabersky@gmail.com>
2022-06-21 15:31:55 +0300

    I fix mistake

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .formLogin()
                 .permitAll()
