715a0fd1553c52d9c7174be1eb6169db0b068ba5
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-06-21 16:07:14 +0300

    Spring security Email Validator homework solution has been added

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,15 @@
 package mate.academy.spring.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import org.springframework.lang.NonNull;
 
 public class MovieSessionRequestDto {
+    @NonNull
     private Long movieId;
+    @NotNull
     private Long cinemaHallId;
+    @NotNull
     private LocalDateTime showTime;
 
     public Long getMovieId() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotBlank
+    @NotEmpty
+    @Size(min = 8)
     private String password;
 
     public String getEmail() {
