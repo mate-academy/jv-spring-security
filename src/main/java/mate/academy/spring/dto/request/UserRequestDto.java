6a132336493276e58f9dbff253d274f5fe32ba9f
Andrii Fabersky <Andriifabersky@gmail.com>
2022-06-21 16:31:14 +0300

    I fix all mistakes

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,7 @@
 package mate.academy.spring.dto.request;
 
-import mate.academy.spring.validation.Email;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
 
 public class UserRequestDto {
     @Email
