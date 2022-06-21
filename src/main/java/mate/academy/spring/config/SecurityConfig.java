5d8352203e103122c5cd8f197e746f2196e107cc
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-06-21 16:26:38 +0300

    hash util deleted

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("vlad")
+                .withUser("vlad@gmail.com")
                 .password(getEncoder().encode("123456"))
                 .roles("USER");
     }
