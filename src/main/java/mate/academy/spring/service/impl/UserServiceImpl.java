d2b6db8eda7f3209a49450a8f43f4803d892f6c3
Vova Klimyuk <klimyuk054@gmail.com>
2022-06-21 13:59:01 +0300

    Added in memory Spring Security, validations annotations. Implement email validation and CustomGlobalExceptionHandler. Refactored AuthenticationService.

--- src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -4,24 +4,24 @@ import java.util.Optional;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.util.HashUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private static final int SALT_LENGTH = 10;
     private final UserDao userDao;
+    private final PasswordEncoder encoder;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder encoder) {
         this.userDao = userDao;
+        this.encoder = encoder;
     }
 
     @Override
     public User add(User user) {
-        String salt = HashUtil.getSalt(SALT_LENGTH);
-        String securePassword = HashUtil.generateSecurePassword(user.getPassword(), salt);
-        user.setPassword(securePassword);
-        user.setSalt(salt);
+        String password = user.getPassword();
+        user.setPassword(encoder.encode(password));
         return userDao.add(user);
     }
 
---
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.validator;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[a-zA-Z\\d.!#$%&'*+/=?^_{|}~-]"
+            + "+@((\\[\\d{1,3}\\.\\d{1,3}.\\[\\d{1,3}."
+            + "\\[\\d{1,3}])|(([a-zA-Z\\-\\d]+\\.)+[a-zA-Z]{2,}))$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return email != null && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+    }
+}
