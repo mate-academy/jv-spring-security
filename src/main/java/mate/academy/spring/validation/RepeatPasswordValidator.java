36af83953fa04f93d865cc5cf7e527d39788b969
oleg-new <9213195@gmail.com>
2022-06-21 10:41:36 +0300

    added missing annotations in the RepeatPassword class fixed variable checked for null extra blank lines removed

--- src/main/java/mate/academy/spring/validation/RepeatPasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/RepeatPasswordValidator.java
@@ -6,11 +6,10 @@ import mate.academy.spring.dto.request.UserRequestDto;
 
 public class RepeatPasswordValidator implements
         ConstraintValidator<RepeatPassword, UserRequestDto> {
-
     @Override
     public boolean isValid(UserRequestDto userRequestDto,
                            ConstraintValidatorContext constraintValidatorContext) {
-        if (userRequestDto.getRepeatPassword() == null) {
+        if (userRequestDto.getPassword() == null) {
             return false;
         }
         return userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
