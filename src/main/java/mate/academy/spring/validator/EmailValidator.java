1dfbf48fe514105843664187ca24f140e43871d2
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-06-23 17:44:01 +0300

    fixed bugs

--- src/main/java/mate/academy/spring/validator/EmailValidator.java
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -3,7 +3,7 @@ package mate.academy.spring.validator;
 import java.util.regex.Pattern;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraints.Email;
+
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final String EMAIL_PATTERN = "^[a-zA-Z\\d.!#$%&'*+/=?^_{|}~-]"
