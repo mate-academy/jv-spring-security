fc3d286974f314a29a291828f5f50cfb9887cb20
Igor <ZIM20093@gmail.com>
2022-07-27 14:53:51 +0300

    first commit

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.validation;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_VALIDATOR = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email == null) {
+            return false;
+        }
+        return Pattern.compile(EMAIL_VALIDATOR).matcher(email).matches();
+    }
+}
