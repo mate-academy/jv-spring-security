78c6d250d86173ae2226158b7b4b89a4f290ac24
Vasylyi Bevz <bevzvasya@gmail.com>
2022-08-10 01:13:11 +0300

    Spring security was done

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,17 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.ValidPasswords;
+
+@ValidPasswords
 public class UserRequestDto {
+    @Email
     private String email;
+    @Size(min = 6)
     private String password;
+
+    private String repeatedPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +19,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatedPassword() {
+        return repeatedPassword;
     }
 }
