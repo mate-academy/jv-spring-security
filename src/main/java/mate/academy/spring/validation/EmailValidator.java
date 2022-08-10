a7d22d3cb102b33feeb9353228d8cffbda21a5bc
Dima Podakin <calombo.podakin@ukr.net>
2022-08-10 11:16:57 +0300

    implemented constant for pattern

--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -8,11 +8,12 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_'{|}~-]"
             + "+@((\\[[0-9]{1,3}\\.[0-9]{1,3}.\\[[0-9]{1,3}."
             + "\\[[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     public boolean isValid(String email,
                            ConstraintValidatorContext constraintValidatorContext) {
         return email != null
-                && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+                && PATTERN.matcher(email).matches();
     }
 }
