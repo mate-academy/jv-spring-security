0e80ff1d4e44a9fda0be7c47c112913f2bc59681
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-08-11 02:11:58 +0300

    Implemented tasks

---
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -0,0 +1,14 @@
+package mate.academy.spring.validator;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return email != null && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+    }
+}
