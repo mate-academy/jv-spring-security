ae5d86ae58958df2e89d65708feb456c817b49f1
Nazar <258258>
2022-08-10 14:37:53 +0300

    added changes after review

--- src/main/java/mate/academy/spring/service/validator/EmailValidator.java
+++ src/main/java/mate/academy/spring/service/validator/EmailValidator.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.service.validator;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -9,15 +8,14 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_'{|}~-]"
             + "+@((\\[[0-9]{1,3}\\.[0-9]{1,3}.\\[[0-9]{1,3}."
             + "\\[[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         if (email == null) {
             return false;
         } else {
-            Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-            Matcher matcher = pattern.matcher(email);
-            return matcher.matches();
+            return PATTERN.matcher(email).matches();
         }
     }
 }
