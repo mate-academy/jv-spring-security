6a2ce8d148c01e13a608c2d015bb906a20238dac
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-08-11 14:46:42 +0300

    add Const and optimize code

--- src/main/java/mate/academy/spring/validator/EmailValidator.java
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -6,9 +6,10 @@ import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final String EMAIL_PATTERN = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        return email != null && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+        return email != null && PATTERN.matcher(email).matches();
     }
 }
