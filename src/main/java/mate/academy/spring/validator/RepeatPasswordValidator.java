beab4322a76f2b4ce94dcc90e4f3fff740f0c68b
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2022-08-18 14:55:19 +0300

    refactor: minor changes

--- src/main/java/mate/academy/spring/validator/RepeatPasswordValidator.java
+++ src/main/java/mate/academy/spring/validator/RepeatPasswordValidator.java
@@ -8,9 +8,6 @@ public class RepeatPasswordValidator
         implements ConstraintValidator<RepeatPassword, UserRequestDto> {
     @Override
     public boolean isValid(UserRequestDto user, ConstraintValidatorContext context) {
-        if (user.getPassword() != null && user.getRepeatPassword() != null) {
-            return user.getPassword().equals(user.getRepeatPassword());
-        }
-        return false;
+        return user.getPassword() != null && user.getPassword().equals(user.getRepeatPassword());
     }
 }
