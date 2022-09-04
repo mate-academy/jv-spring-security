718d24906ea73aaac1b196093e20d2b41f37ca9d
drapail <mv.dibrov@gmail.com>
2022-09-04 23:35:52 +0300

    Spring Security configured

---
+++ src/main/java/mate/academy/spring/config/SecurityCOnfig.java
@@ -0,0 +1,8 @@
+package mate.academy.spring.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityCOnfig extends WebSecurityConfigurerAdapter {
+}
