deb9f5bb167c97e252ac6d4b323a7351794fa0c2
AndriiP <andrey.pslk@gmail.com>
2022-09-06 19:33:25 +0300

    first commit

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 public class MovieRequestDto {
+    @NotNull
     private String title;
+    @NotNull
+    @Length(min = 100)
     private String description;
 
     public String getTitle() {
