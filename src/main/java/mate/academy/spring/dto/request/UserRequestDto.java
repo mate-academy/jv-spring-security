c4982f08489c428d6a08eee065ae211b5da73ed4
AndriiP <andrey.pslk@gmail.com>
2022-09-06 20:23:57 +0300

    implemented except creating custom response entity with status 500

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,12 +2,19 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.NotNull;
 import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
+import org.hibernate.validator.constraints.Length;
 
+@Password
 public class UserRequestDto {
     @Email
     private String email;
     @NotNull
+    @Length(min = 4)
     private String password;
+    @NotNull
+    @Length(min = 4)
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -15,5 +22,13 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
