f9facd9287269776993e92ee9ade3c915ab5b1b9
drapail <mv.dibrov@gmail.com>
2022-09-12 23:48:14 +0300

    replaced ?userId @RequestParam with Authentication object

--- src/main/java/mate/academy/spring/config/SecurityCOnfig.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.spring.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityCOnfig extends WebSecurityConfigurerAdapter {
-}
--- src/main/java/mate/academy/spring/controller/OrderController.java
+++ src/main/java/mate/academy/spring/controller/OrderController.java
@@ -2,6 +2,8 @@ package mate.academy.spring.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import mate.academy.spring.config.SecurityConfig;
 import mate.academy.spring.dto.response.OrderResponseDto;
 import mate.academy.spring.model.Order;
 import mate.academy.spring.model.ShoppingCart;
@@ -9,6 +11,8 @@ import mate.academy.spring.service.OrderService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.ResponseDtoMapper;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,13 +38,17 @@ public class OrderController {
     }
 
     @PostMapping("/complete")
-    public OrderResponseDto completeOrder(@RequestParam Long userId) {
+    public OrderResponseDto completeOrder() {
+        Long userId = userService.findByEmail(SecurityContextHolder.getContext()
+                .getAuthentication().getName()).get().getId();
         ShoppingCart cart = shoppingCartService.getByUser(userService.get(userId));
         return orderResponseDtoMapper.mapToDto(orderService.completeOrder(cart));
     }
 
     @GetMapping
-    public List<OrderResponseDto> getOrderHistory(@RequestParam Long userId) {
+    public List<OrderResponseDto> getOrderHistory() {
+        Long userId = userService.findByEmail(SecurityContextHolder.getContext()
+                .getAuthentication().getName()).get().getId();
         return orderService.getOrdersHistory(userService.get(userId))
                 .stream()
                 .map(orderResponseDtoMapper::mapToDto)
