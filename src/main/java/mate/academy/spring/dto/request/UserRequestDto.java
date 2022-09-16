aba8a4499f4e5faacdaf06d405e58d1e0eedac35
Ilya <zimmboco@gmail.com>
2022-09-16 17:19:00 +0300

    add SecurityConfig

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,6 +2,7 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import mate.academy.spring.validator.Email;
 import mate.academy.spring.validator.RepeatPassword;
 
@@ -10,10 +11,10 @@ public class UserRequestDto {
     @Email
     private String email;
     @NotNull
-    @Min(8)
+    @Size(min = 8)
     private String password;
     @NotNull
-    @Min(8)
+    @Size(min = 8)
     private String repeatPassword;
 
     public String getEmail() {
