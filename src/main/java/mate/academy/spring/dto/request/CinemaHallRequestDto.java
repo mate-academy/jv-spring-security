8b797528d1234379bac78b8edfeb593b199e1a4b
Vitalii Smahlenko <smaglenko@gmail.com>
2022-09-16 11:11:20 +0300

    Added @NonNull, @Min & Custom email validation implemented

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Min(1)
     private int capacity;
+    @NotNull
+    @Min(4)
     private String description;
 
     public int getCapacity() {
