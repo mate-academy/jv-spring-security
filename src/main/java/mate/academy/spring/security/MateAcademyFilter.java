34b2817c410ef3ab10264c3d47d55d712870bcd2
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-09-18 21:02:02 +0300

    spring security implemented

---
+++ src/main/java/mate/academy/spring/security/MateAcademyFilter.java
@@ -0,0 +1,20 @@
+package mate.academy.spring.security;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class MateAcademyFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("MateAcademyFilter filter doFilter activated");
+        filterChain.doFilter(servletRequest,servletResponse);
+
+    }
+}
