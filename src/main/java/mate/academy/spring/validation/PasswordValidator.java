70493506995c35eb4ba1d4293e2190bae3fd7286
agash <alexgash8@gmail.com>
2022-09-26 23:06:45 +0300

    with password matcher next try

--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -6,7 +6,11 @@ import mate.academy.spring.dto.request.UserRequestDto;
 import mate.academy.spring.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
+    private static final String PASSWORD_PATTERN = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
     private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
@@ -17,9 +21,15 @@ public class PasswordValidator implements ConstraintValidator<Password, UserRequ
 
     @Override
     public boolean isValid(UserRequestDto userRequestDto, ConstraintValidatorContext context) {
-        if (userRequestDto.getPassword() == userRequestDto.getRepeatedPassword()) {
-            return userService.findByEmail(userRequestDto.getEmail()).get().getPassword()
-                    == passwordEncoder.encode(userRequestDto.getPassword());
+        if (userRequestDto.getPassword() != null
+                && userRequestDto.getRepeatedPassword() != null
+                && userRequestDto.getPassword().equals(userRequestDto.getRepeatedPassword())) {
+            Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+            Matcher matcher = pattern.matcher(userRequestDto.getPassword());
+            if (matcher.matches()) {
+                return true;
+            }
+            return false;
         } else {
             return false;
         }
