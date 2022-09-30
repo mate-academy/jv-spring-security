9f4f6d8f947051ea7d7e265876e35525f64484bd
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-09-30 21:02:33 +0200

    MovieSessionRequestDto-corrected

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -3,7 +3,6 @@ package mate.academy.spring.dto.request;
 import java.time.LocalDateTime;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
 
 public class MovieSessionRequestDto {
     @NotNull(message = "Please enter a movie id")
@@ -13,7 +12,6 @@ public class MovieSessionRequestDto {
     @Min(value = 1)
     private Long cinemaHallId;
     @NotNull(message = "Please enter a show time")
-    @DateTimeFormat(pattern = "2007-12-03T10:15:30")
     private LocalDateTime showTime;
 
     public Long getMovieId() {
