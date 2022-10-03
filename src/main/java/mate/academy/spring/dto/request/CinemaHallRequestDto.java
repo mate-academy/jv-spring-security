c5e13a6aaaa5a303d81cd37da34b31306401c7a2
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-10-03 17:57:17 +0300

    Validation of data for models is implemented

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Min(value = 0)
     private int capacity;
+    @NotNull
     private String description;
 
     public int getCapacity() {
