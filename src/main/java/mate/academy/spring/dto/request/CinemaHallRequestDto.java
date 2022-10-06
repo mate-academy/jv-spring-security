cab3c5c0609223cdacff44d495c92188c35ad10c
Valerii-Sh <val-sh@ukr.net>
2022-10-06 11:11:06 +0300

    HW jv-spring-security is done

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import org.hibernate.validator.constraints.Length;
+
 public class CinemaHallRequestDto {
+    @Min(value = 11, message = "The Capacity should be > 10.")
     private int capacity;
+    @Length(max=255, message = "The Description should be < 255 characters.")
     private String description;
 
     public int getCapacity() {
