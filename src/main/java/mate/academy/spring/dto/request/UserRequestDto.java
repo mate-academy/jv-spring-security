993645eb3435d6ea86e0ed38ec3473eb4158e202
Slava Verbanov <v.verbanov@gmail.com>
2022-10-06 23:39:32 +0200

    add validation annotations

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @NotNull
+    @Size(min = 4)
     private String email;
+    @NotNull
+    @Size(min = 4)
     private String password;
 
     public String getEmail() {
