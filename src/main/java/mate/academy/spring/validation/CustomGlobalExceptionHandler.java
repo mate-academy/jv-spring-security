8eece836dc99fddadafbcef19b0122a4681db5d4
GolDenik <denik.golibroda@gmail.com>
2022-10-19 19:51:30 +0300

    upgraded solution

--- src/main/java/mate/academy/spring/validation/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/validation/CustomGlobalExceptionHandler.java
@@ -27,10 +27,7 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern(DateTimePatternUtil.DATE_TIME_PATTERN)));
-        body.put("status", status.value());
+        Map<String, Object> body = prepareBodyWithStatusAndTimestamp(status.value());
         List<String> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(this::getErrorMessage)
                 .collect(Collectors.toList());
@@ -40,13 +37,18 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 
     @ExceptionHandler(value = DataProcessingException.class)
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        Map<String, Object> body = prepareBodyWithStatusAndTimestamp(500);
         DataProcessingException dataProcessingException = (DataProcessingException) ex;
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpHeaders.EMPTY, HttpStatus.resolve(500));
+    }
+
+    private Map<String, Object> prepareBodyWithStatusAndTimestamp(int status) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern(DateTimePatternUtil.DATE_TIME_PATTERN)));
-        body.put("status", 500);
-        body.put("error", ex.getMessage());
-        return new ResponseEntity<>(body, HttpHeaders.EMPTY, HttpStatus.resolve(500));
+        body.put("status", status);
+        return body;
     }
 
     private String getErrorMessage(ObjectError error) {
