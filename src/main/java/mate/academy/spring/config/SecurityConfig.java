b42dfdcb4f525a03a0f856438b6c479bb46508bf
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-10-20 17:11:21 +0300

    Changed vesion in pom Removed artifacts (salt, hashUtil, filter class)

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.config;
 
-import mate.academy.spring.security.MateAcademyFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,7 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .addFilterAfter(new MateAcademyFilter(), BasicAuthenticationFilter.class)
                 .formLogin()
                 .permitAll()
                 .and()
