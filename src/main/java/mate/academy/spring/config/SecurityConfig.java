053f71a747b9cedabd0e4f0de298419e3c0e7b25
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-21 23:20:35 +0200

    implement inMemoryAuthentication

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,8 +1,16 @@
 package mate.academy.spring.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("bob")
+                .password("1234")
+                .roles("USER");
+    }
 }
