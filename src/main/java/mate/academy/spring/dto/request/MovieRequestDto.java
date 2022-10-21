ac25b8db8bc793f70c4a9274e3e7d8a532cfc511
ArturZhadan <artur.zhadan.17@gmail.com>
2022-10-21 16:16:30 +0300

    configured Spring Security with in memory authentication, implemented email validation and CustomGlobalExceptionHandler

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull(message = "Please enter title")
+    @Size(min = 5, max = 10, message = "Title size must be min=5 and max=10")
     private String title;
+    @NotNull(message = "Please enter description")
+    @Size(min = 5, max = 10, message = "Description size must be min=5 and max=10")
     private String description;
 
     public String getTitle() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @NotNull(message = "Please enter email")
+    @Size(min = 5, max = 15, message = "Email size must be min=5 and max=15")
+    @Email
     private String email;
+    @NotNull(message = "Please enter password")
+    @Size(min = 5, max = 15, message = "Password size must be min=5 and max=15")
     private String password;
 
     public String getEmail() {
