0c06cb9fe2aded659abf4b803a562d103f41db02
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-21 17:02:28 +0200

    Connect to DB

---
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package mate.academy.spring.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
