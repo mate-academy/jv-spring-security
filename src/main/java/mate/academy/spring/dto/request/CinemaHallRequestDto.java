f503cbc24f42033a8dc8fc0889515279f57e12d1
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-10-24 00:55:56 +0300

    Was added annotations

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import org.springframework.lang.NonNull;
+
 public class CinemaHallRequestDto {
+    @Min(value = 1)
     private int capacity;
+    @NonNull
+    @Size(min = 100)
     private String description;
 
     public int getCapacity() {
