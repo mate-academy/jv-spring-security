cc05fa8926c975dbc8b417fcd15f6e0cd80f1a29
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-10-24 14:20:32 +0300

    corrected menthor's comments

--- src/main/java/mate/academy/spring/validation/Password.java
+++ src/main/java/mate/academy/spring/validation/Password.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = PasswordValidator.class)
 @Target({ElementType.TYPE})
@@ -16,9 +17,7 @@ public @interface Password {
 
     String repeatPassword();
 
-    @Target({ElementType.TYPE})
-    @Retention(RetentionPolicy.RUNTIME)
-    @interface List {
-        Password[] value();
-    }
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
