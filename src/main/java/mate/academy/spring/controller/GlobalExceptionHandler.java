d156cc86b59216238698c10641249236854059c0
Misha Brovkin <haasiakira@gmail.com>
2022-10-24 12:38:54 +0300

    fix some style issues

--- src/main/java/mate/academy/spring/controller/GlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/controller/GlobalExceptionHandler.java
@@ -20,9 +20,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now().toString());
         body.put("status", status.value());
