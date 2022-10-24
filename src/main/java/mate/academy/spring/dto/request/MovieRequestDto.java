92d6f8f37d509627dec4339976bb50f2c4567b55
Danylo Frolov <frolovdanilik@gmail.com>
2022-10-24 14:39:23 +0300

    solution

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @Size(min = 3, message = "Title length can not be less than 3")
     private String title;
+    @NotBlank
     private String description;
 
     public String getTitle() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public class UserRequestDto {
+    @Email(message = "Invalid email",
+            regexp = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$")
     private String email;
+    @Size(min = 4, message = "Password length must not be less than 4")
     private String password;
 
     public String getEmail() {
