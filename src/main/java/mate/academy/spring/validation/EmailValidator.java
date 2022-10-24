d0093a4971b816c5079b3e3021dd023f4264256a
Danylo Frolov <frolovdanilik@gmail.com>
2022-10-24 16:35:08 +0300

    applied changes

--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -4,10 +4,11 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String REGEX = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
+    private static final String EMAIL_PATTERN =
+            "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return false;
+        return value != null && value.matches(EMAIL_PATTERN);
     }
 }
