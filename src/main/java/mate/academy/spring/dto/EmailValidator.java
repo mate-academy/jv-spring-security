38ebbc218ba48258106dfc401f1cc07c167d7448
LubusUzh <lubus.uzh@gmail.com>
2022-10-24 15:19:20 +0300

    Add: custom email validation and @Valid annotation

---
+++ src/main/java/mate/academy/spring/dto/EmailValidator.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.dto;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Email;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]"
+            + "+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]"
+            + "+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
