421c3112ab433de438cac4d72e99fc5c5f9132aa
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-10-24 15:17:45 +0300

    removed redundant constant

--- src/main/java/mate/academy/spring/validator/EmailValidator.java
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -8,7 +8,6 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_'{|}~-]"
             + "+@((\\[[0-9]{1,3}\\.[0-9]{1,3}.\\[[0-9]{1,3}."
             + "\\[[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-    private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
