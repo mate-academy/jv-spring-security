e5a4b6e7605ffcfcd5b6f03e49f23fab023f71f2
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-10-24 14:27:12 +0300

    Security logics was implemented

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+
 public class MovieRequestDto {
+    @NotNull
     private String title;
+
     private String description;
 
     public String getTitle() {
