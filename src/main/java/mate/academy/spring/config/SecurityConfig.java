8197fa467788eb4c0f57dc2193fc73ec0238349b
LubusUzh <lubus.uzh@gmail.com>
2022-10-24 14:44:30 +0300

    Add: implementation to the SecurityConfig class

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,8 +1,31 @@
 package mate.academy.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication().withUser("bob")
+                .password(getEncoder().encode("1234")).roles("USER");
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
 }
