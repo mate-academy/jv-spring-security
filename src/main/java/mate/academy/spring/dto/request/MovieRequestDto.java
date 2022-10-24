692e0f94c5b708210626f803bd67d9bb318894bb
EagleAndrew <tortloveduck4@gmail.com>
2022-10-24 14:40:03 +0200

    spring security

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @Size(min = 10)
+    @NotNull
     private String title;
+    @Size(min = 10)
+    @NotNull
     private String description;
 
     public String getTitle() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @Email
+    @Size(min = 10, max = 50)
     private String email;
+    @NotNull
+    @Size(min = 8)
     private String password;
 
     public String getEmail() {
