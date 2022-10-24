a304e3f035c6f09afd8c8c84c54eb6bbd1c5fd0a
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-10-24 10:04:11 +0300

    common-mistakes

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,7 +2,6 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Password;
 
@@ -14,7 +13,6 @@ import mate.academy.spring.validation.Password;
 public class UserRequestDto {
     @Email
     private String email;
-    @NotNull
     @NotBlank
     @Size(min = 6)
     private String password;
