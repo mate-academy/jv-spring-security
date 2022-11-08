6da5ba199999fdfaadd2743c7d6cfadca07960f3
Vladyslav Kondiievskyi <80918559+Vladyslav-Kondiievskyi@users.noreply.github.com>
2022-11-08 22:35:05 +0200

    jv-spring-security completed

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @Min(0)
     private int capacity;
+    @Size(min = 2)
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,14 +1,40 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import mate.academy.spring.validation.AuthValid;
+import mate.academy.spring.validation.Email;
+import org.springframework.lang.NonNull;
+
+@AuthValid(message = "passwords are not the same")
 public class UserRequestDto {
+    @Email
     private String email;
+    @NonNull
+    @Min(8)
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
