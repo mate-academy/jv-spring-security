b8d712a27bd89f251e8baa8b1ad7c368dc11e766
Roman Voitalik <voitalik1@gmail.com>
2022-11-25 13:13:30 +0200

    add GlobalExceptionHandler

---
+++ src/main/java/mate/academy/spring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package mate.academy.spring.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        return ResponseEntity.ok().build();
+    }
+}
