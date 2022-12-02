847da262020003fd85c71374daf501b2730b97c6
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-12-02 15:33:08 +0200

    implemented custom email validation, replaced userId parameters with Authentication object

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,17 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public class MovieSessionRequestDto {
+    @NotNull
+    @Min(value = 0)
     private Long movieId;
+    @NotNull
     private Long cinemaHallId;
+    @NotNull
+    @Min(value = 0)
     private LocalDateTime showTime;
 
     public Long getMovieId() {
