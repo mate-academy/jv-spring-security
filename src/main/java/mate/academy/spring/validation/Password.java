12213adcf748dc839d16d144c0a2e5827628994a
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-12-07 00:30:27 +0100

    initial2

---
+++ src/main/java/mate/academy/spring/validation/Password.java
@@ -0,0 +1,13 @@
+package mate.academy.spring.validation;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+}
---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,22 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+    public static final String PASSWORD_PATTERN =
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,}$";
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        if (password == null) {
+            return false;
+        }
+        return true;
+      /*  Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();*/
+    }
+}
