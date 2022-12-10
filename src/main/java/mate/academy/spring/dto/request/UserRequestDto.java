6494ffe58a726cfbdd7a0aa6a7c12d2c5275c00e
Yura Hryniv <superyura444@gmail.com>
2022-12-10 15:22:18 +0200

    Spring security is implemented

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,23 @@
 package mate.academy.spring.dto.request;
 
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Password.List({
+        @Password(first = "password", second = "repeatedPassword",
+                message = "The password fields must match")
+})
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 8, max = 25)
     private String password;
+    @NotNull
+    @Size(min = 8, max = 25)
+    private String repeatedPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +25,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatedPassword() {
+        return repeatedPassword;
     }
 }
