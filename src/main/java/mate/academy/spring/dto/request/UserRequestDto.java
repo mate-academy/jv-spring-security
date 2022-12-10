5cadb220f074f1e82d9b39102201801cc0a31920
Yura Hryniv <superyura444@gmail.com>
2022-12-10 15:24:24 +0200

    Changed order of imports

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,9 +1,9 @@
 package mate.academy.spring.dto.request;
 
-import mate.academy.spring.validation.Email;
-import mate.academy.spring.validation.Password;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
 
 @Password.List({
         @Password(first = "password", second = "repeatedPassword",
