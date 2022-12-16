519e94cfe255de0ae1c48ff1c3ef7177f30034df
galenkoigor <galenkoigor@gmail.com>
2022-12-16 21:02:22 +0100

    Added annotations for validation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.ValidEmail;
+import mate.academy.spring.validation.ValidPassword;
+
+@ValidPassword
 public class UserRequestDto {
+    @ValidEmail
     private String email;
+    @Size(min = 5, max = 10)
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +18,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
---
+++ src/main/java/mate/academy/spring/validation/ValidPassword.java
@@ -0,0 +1,21 @@
+package mate.academy.spring.validation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({TYPE})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "Passwords must be equal";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
