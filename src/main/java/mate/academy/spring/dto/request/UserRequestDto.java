09ce846480432668f2c090a0c9423419f6d30b9e
GrishaSeniv <seniv.grisha@gmail.com>
2022-12-20 23:04:07 +0200

    implemented custom email validation, CustomGlobalExceptionHandler, added valid annotations

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotEmpty
     private String password;
 
     public String getEmail() {
