0c5c4d5939685a9d65693cb6d752460a7e3d45e4
Anatolii Bespalko <tolyan9901@gmail.com>
2022-12-20 15:29:01 +0200

    common fix

--- src/main/java/mate/academy/spring/validation/MatchesPasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/MatchesPasswordValidator.java
@@ -8,10 +8,6 @@ import mate.academy.spring.dto.request.UserRequestDto;
 public class MatchesPasswordValidator implements ConstraintValidator<RepeatedPasswordEquals,
         UserRequestDto> {
     @Override
-    public void initialize(RepeatedPasswordEquals constraintAnnotation) {
-    }
-
-    @Override
     public boolean isValid(UserRequestDto userRequestDto, ConstraintValidatorContext context) {
         return Objects.equals(userRequestDto.getRepeatPassword(), userRequestDto.getPassword());
     }
