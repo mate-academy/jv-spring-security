b968869847895af07b514a4eaa35ea0d6a6f4b83
Oleksiy <a.o.masaltsev@gmail.com>
2022-12-20 23:42:36 +0200

    Authentication added, constrains annotations

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 
 public class MovieSessionRequestDto {
+    @PositiveOrZero
     private Long movieId;
+    @PositiveOrZero
     private Long cinemaHallId;
+    @NotNull
     private LocalDateTime showTime;
 
     public Long getMovieId() {
