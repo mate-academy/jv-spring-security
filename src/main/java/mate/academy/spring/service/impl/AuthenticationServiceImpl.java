5f94d92f4e833eac3ba0e31bd6852826fff33bc1
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-12-20 22:48:12 +0200

    Removed login()

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -6,7 +6,6 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.util.HashUtil;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,16 +17,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
-    }
-
-    @Override
-    public User login(String email, String password) throws AuthenticationException {
-        Optional<User> user = userService.findByEmail(email);
-        if (user.isPresent() && user.get().getPassword().equals(
-                HashUtil.generateSecurePassword(password, user.get().getSalt()))) {
-            return user.get();
-        }
-        throw new AuthenticationException("Invalid login or password");
     }
 
     @Override
