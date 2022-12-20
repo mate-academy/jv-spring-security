9f1c051563a6a1d213ab6b02d1ba5497757762ac
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-12-20 21:56:19 +0200

    Added valid annotation

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class MovieSessionRequestDto {
+    @Min(value = 1)
     private Long movieId;
+    @Min(value = 1)
     private Long cinemaHallId;
+    @NotNull
     private LocalDateTime showTime;
 
     public Long getMovieId() {
