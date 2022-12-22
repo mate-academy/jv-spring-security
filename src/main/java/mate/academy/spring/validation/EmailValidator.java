1b107fb04f119e7d600a1b43df8dec033b27d414
thebitler <yurchyknikita@gmail.com>
2022-12-22 13:49:25 +0100

    renamed argument in isValid()

--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -10,10 +10,10 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
     private static final int RFC5321_MAX_PATH_LENGTH = 254;
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null || value.isEmpty() || value.length() > RFC5321_MAX_PATH_LENGTH) {
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if (email == null || email.isEmpty() || email.length() > RFC5321_MAX_PATH_LENGTH) {
             return false;
         }
-        return PATTERN.matcher(value).matches();
+        return PATTERN.matcher(email).matches();
     }
 }
