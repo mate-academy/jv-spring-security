332bb73c7ed2d4ff56734d5e26c8a25c129cc049
thebitler <yurchyknikita@gmail.com>
2022-12-22 13:51:16 +0100

    added obligatory parameters to validator annotations

--- src/main/java/mate/academy/spring/validation/ValidPassword.java
+++ src/main/java/mate/academy/spring/validation/ValidPassword.java
@@ -5,9 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = EmailValidator.class)
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
 }
