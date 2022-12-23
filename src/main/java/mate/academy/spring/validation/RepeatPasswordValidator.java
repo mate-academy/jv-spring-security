ad585b09f3adef9af37d6869774cc380c8777750
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-12-23 11:47:57 +0200

    Done

---
+++ src/main/java/mate/academy/spring/validation/RepeatPasswordValidator.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+import mate.academy.spring.validation.lib.RepeatPassword;
+
+public class RepeatPasswordValidator implements
+        ConstraintValidator<RepeatPassword,UserRequestDto> {
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        return userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
+    }
+}
