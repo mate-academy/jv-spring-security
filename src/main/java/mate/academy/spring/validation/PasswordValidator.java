894d9a4ad4b2480d4b97200c3d31d9e5d171cb5d
Andrii Matveev <matveev.tesla@gmail.com>
2022-12-23 17:57:42 +0200

    homework has been done

---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.validation;
+
+import java.util.Objects;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, Object> {
+    private String password;
+    private String repeatPassword;
+
+    @Override
+    public void initialize(ValidPassword constraintAnnotation) {
+        password = constraintAnnotation.passwordField();
+        repeatPassword = constraintAnnotation.repeatPasswordField();
+    }
+
+    @Override
+    public boolean isValid(Object pass, ConstraintValidatorContext constraintValidatorContext) {
+        Object passwordValue = new BeanWrapperImpl(pass).getPropertyValue(password);
+        Object repeatPasswordValue = new BeanWrapperImpl(pass).getPropertyValue(repeatPassword);
+        return Objects.equals(passwordValue, repeatPasswordValue);
+    }
+}
