8528caa67657852105d95c3adf7fa4bb049ebdde
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-12-23 18:35:41 +0200

    Corrected

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,11 +1,9 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 public class MovieRequestDto {
     @NotNull
-    @Max(50)
     private String title;
     private String description;
 
--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -7,16 +7,16 @@ import javax.validation.ConstraintValidatorContext;
 import mate.academy.spring.validation.lib.Email;
 
 public class EmailValidator implements ConstraintValidator<Email,String> {
-    private static final String EMAIL_PATTERN = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+    private static final Pattern EMAIL_PATTERN = Pattern
+            .compile("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         if (email == null) {
             return false;
         }
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
         return matcher.matches();
     }
 }
-
