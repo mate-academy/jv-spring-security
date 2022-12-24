76f8368b5f8ed6c1b6c746e0d1dee610aafc1f30
NSay0 <krvipa@gmail.com>
2022-12-24 21:37:16 +0200

    Corrections after StReview

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register").permitAll()
+                .antMatchers("/register").anonymous()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
