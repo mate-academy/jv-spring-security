1e6606d9d22f6c747988aebede4481a53709c7cc
Serhii Prykhodko <sergii.prykhodko@gmail.com>
2022-12-28 20:14:37 +0200

    deleted old fork, create new and try to pull request

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+
 public class CinemaHallRequestDto {
+    @Min(2)
     private int capacity;
+    @Min(10)
     private String description;
 
     public int getCapacity() {
