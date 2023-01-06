5de54efb1af9330d10aa0870ff8588db494826aa
Alexander <aleksandr_pogreb@ukr.net>
2023-01-06 14:46:15 +0200

    solution

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,19 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.RepeatPassword;
+
+@RepeatPassword
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 8, max = 16)
     private String password;
+    @Size(min = 8, max = 16)
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +21,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
