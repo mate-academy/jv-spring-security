94724970ba48efaf85640e1915c7d368432e779f
Katya Humeniuk <kapelka.boo@gmail.com>
2023-01-23 15:35:24 +0200

    Mistakes were fixed.

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -35,5 +35,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable();
     }
-
 }
--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -5,7 +5,6 @@ import javax.validation.ConstraintValidatorContext;
 import mate.academy.spring.dto.request.UserRequestDto;
 
 public class PasswordValidator implements ConstraintValidator<ValidPassword, UserRequestDto> {
-
     @Override
     public boolean isValid(UserRequestDto userRequestDto, ConstraintValidatorContext context) {
         if (userRequestDto.getPassword() == null || userRequestDto.getRepeatedPassword() == null) {
