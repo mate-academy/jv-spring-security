3e6a227cdfdd296cae68061e7a9ce7250e5562be
AndriySichniy <sichniy.andriy@gmail.com>
2023-02-05 16:37:54 +0200

    Add annotations for dto objects

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import mate.academy.spring.validation.Password;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @NotNull
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 4)
+    @Password
     private String password;
 
     public String getEmail() {
