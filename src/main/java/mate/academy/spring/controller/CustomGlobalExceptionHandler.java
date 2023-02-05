24f9aeece5d0f0dd39b8cd74ca44f39f4ee9485b
AndriySichniy <89684976+SichniyAndriy@users.noreply.github.com>
2023-02-05 20:02:02 +0200

    Update src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
@@ -22,10 +22,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    private DataProcessingException ex;
-    private HttpHeaders headers;
-    private HttpStatus status;
-    private WebRequest request;
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
