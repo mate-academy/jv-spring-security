96da2d27039495d282db791f40569036d0bf67e4
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-12 21:35:47 +0200

    Configure inMemory authentication

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,9 +1,39 @@
 package mate.academy.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("max")
+                .password(getEncoder().encode("max"))
+                .roles("USER")
+                .and()
+                .withUser("marina")
+                .password(getEncoder().encode("marina"))
+                .roles("ADMIN");
+    }
 
+    @Override
+    protected void configure(HttpSecurity req) throws Exception {
+        req.authorizeRequests()
+                .antMatchers("/users").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
