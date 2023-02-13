5c2fabe33826ed109e4a27568c0d1e80623de021
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-13 23:21:52 +0200

    Add Repeat Password validation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -3,7 +3,11 @@ package mate.academy.spring.dto.request;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.EmailValidation;
+import mate.academy.spring.validation.MatchFields;
 
+@MatchFields(first = "password",
+        second = "repeatPassword",
+        message = "Passwords must be equal!")
 public class UserRequestDto {
     @NotNull
     @EmailValidation
@@ -12,11 +16,19 @@ public class UserRequestDto {
     @Size(min = 5)
     private String password;
 
+    @NotNull
+    @Size(min = 5)
+    private String repeatPassword;
+
     public String getEmail() {
         return email;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
---
+++ src/main/java/mate/academy/spring/validation/MatchFieldsValidator.java
@@ -0,0 +1,20 @@
+package mate.academy.spring.validation;
+
+import java.util.Objects;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class MatchFieldsValidator implements ConstraintValidator<MatchFields, Object> {
+    private final String firstField;
+    private final String secondField;
+
+    public MatchFieldsValidator(MatchFields annotation) {
+        this.firstField = annotation.first();
+        this.secondField = annotation.second();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        return (!Objects.equals(firstField, secondField) || firstField != null) && firstField.equals(secondField);
+    }
+}
