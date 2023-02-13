610ad2ac0dafb5671da6e089cf749cc49eb94daf
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-13 21:13:46 +0200

    Add Fields validation

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @Min(value = 10)
     private int capacity;
+    @NotBlank
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.EmailValidation;
+
 public class UserRequestDto {
+    @NotNull
+    @EmailValidation
     private String email;
+    @NotNull
+    @Size(min = 5)
     private String password;
 
     public String getEmail() {
