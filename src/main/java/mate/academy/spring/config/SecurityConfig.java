6aee8024d11e64265522b4e9cd402fb70de2d96e
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-14 23:53:05 +0200

    Add basic authentication

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package mate.academy.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,13 +13,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
         builder.inMemoryAuthentication()
-                .withUser("max")
-                .password(getEncoder().encode("max@max.com"))
+                .withUser("max@max.com")
+                .password(getEncoder().encode("maximMaxim"))
                 .roles("USER")
                 .and()
                 .withUser("marina@marina.com")
-                .password(getEncoder().encode("marina"))
+                .password(getEncoder().encode("marinaMarina"))
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
     @Bean
