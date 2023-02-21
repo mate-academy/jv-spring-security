0e64d85489a33f97ca11000a7c5cf6a2e1e70ad4
Maxim Surzhik <kabaneri15@gmail.com>
2023-02-21 22:18:38 +0200

    changes2

--- src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
@@ -36,10 +36,8 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     }
 
     @ExceptionHandler(DataProcessingException.class)
-    protected ResponseEntity<Object> handleMethodDpeCatch(MethodArgumentNotValidException e,
-                                                                               HttpHeaders headers,
-                                                                               HttpStatus status,
-                                                                               WebRequest request) {
+    protected ResponseEntity<Object> handleDataProcessingException(MethodArgumentNotValidException e,
+                                                                               HttpHeaders headers) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now().toString());
         body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
