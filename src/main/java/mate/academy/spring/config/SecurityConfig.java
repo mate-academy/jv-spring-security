c229e4bbdda9b239e68bc97fbfe48c4d521eaa71
Maks Hrytsyna <hrytsynamax@gmail.com>
2023-03-01 17:38:45 +0200

    SecurityConfig fixed

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,22 +1,26 @@
 package mate.academy.spring.config;
 
 import java.util.Properties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("maks@gmail.com")
-                .password(getPassWordEncoder().encode("1234"))
+                .password(passwordEncoder.encode("1234"))
                 .roles("USER");
     }
 
@@ -36,14 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         Properties users = new Properties();
         users.put("alice.smith@gmail.com",
-                getPassWordEncoder().encode("12345678") + ",ROLE_USER,enabled");
+                passwordEncoder.encode("12345678") + ",ROLE_USER,enabled");
         users.put("bob.brown@gmail.com",
-                getPassWordEncoder().encode("qwertyuiop") + ",ROLE_USER,enabled");
+                passwordEncoder.encode("qwertyuiop") + ",ROLE_USER,enabled");
         return new InMemoryUserDetailsManager(users);
-    }
-
-    @Bean
-    public PasswordEncoder getPassWordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
