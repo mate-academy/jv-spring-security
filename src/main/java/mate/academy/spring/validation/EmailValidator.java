abbd6086ed796a055724872a32eb6021cc255db8
Dastya <daniillipenec@gmail.com>
2023-03-02 17:01:20 +0200

    security commit

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,14 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\\\+]+"
+            + "(\\\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$";
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return s != null && s.matches(EMAIL_PATTERN);
+    }
+}
