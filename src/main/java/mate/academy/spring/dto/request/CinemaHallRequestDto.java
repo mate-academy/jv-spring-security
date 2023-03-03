52bd13eb3b1ca7f0e4be24120f0661f3e26f292a
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-03-03 12:21:24 +0200

    Spring Security 1st Commit

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,13 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class CinemaHallRequestDto {
     private int capacity;
     private String description;
 
+    @Min(0)
     public int getCapacity() {
         return capacity;
     }
 
+    @NotNull
     public String getDescription() {
         return description;
     }
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
     private String password;
 
     public String getEmail() {
