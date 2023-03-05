7dce4d3e9197fd17daeef31aaa439fdf9b61d51e
Serhii Berdychevskyi <sirko120690@mail.com>
2023-03-06 00:54:41 +0200

    Added second version spring-security

--- src/main/java/mate/academy/spring/controller/GlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/controller/GlobalExceptionHandler.java
@@ -1,26 +1,28 @@
 package mate.academy.spring.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now().toString());
         body.put("status", status.value());
@@ -28,16 +30,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .getAllErrors()
                 .stream()
                 .map(this::getErrorMessage)
-                .toList();
+                .collect(Collectors.toList());
         body.put("errors", errors);
         return new ResponseEntity<>(body, headers, status);
     }
 
-    private String getErrorMessage(ObjectError exception) {
-        if (exception instanceof FieldError) {
-            String field = ((FieldError) exception).getField();
-            return field + " " + exception.getDefaultMessage();
+    private String getErrorMessage(ObjectError error) {
+        if (error instanceof FieldError) {
+            String field = ((FieldError) error).getField();
+            return field + " " + error.getDefaultMessage();
         }
-        return exception.getDefaultMessage();
+        return error.getDefaultMessage();
     }
 }
