6b04acb98c54ba5c3ac2e58d88c702dd414a7754
Serhii Berdychevskyi <sirko120690@mail.com>
2023-03-06 13:50:02 +0200

    Added changes second ver. spring - security

--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -5,7 +5,8 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}"
+    private static final Pattern EMAIL_REGEX
+            = Pattern.compile("^[a-zA-z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}"
             + "\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
 
     @Override
