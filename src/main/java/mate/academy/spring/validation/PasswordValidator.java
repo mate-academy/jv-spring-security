548107cf637df6532c1510f85f276423a329e96b
Maks Untilov <maksimuntilov7@gmail.com>
2023-03-17 02:08:45 +0200

    Hw spring security was done

---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto, ConstraintValidatorContext validator) {
+        if (userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword())) {
+            return true;
+        }
+        return false;
+    }
+}
