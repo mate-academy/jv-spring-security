053720d729d3849969daa9bc0ac252bb3eeba865
Roman Usyk <roman.usik.93@ukr.net>
2023-03-20 17:08:30 +0200

    implemented all classes and  methods by task

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,21 @@
 package mate.academy.spring.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class MovieSessionRequestDto {
+    @NotNull
+    @Min(value = 0)
     private Long movieId;
+
+    @NotNull
+    @Min(value = 0)
     private Long cinemaHallId;
+
+    @NotNull
+    @DateTimeFormat
     private LocalDateTime showTime;
 
     public Long getMovieId() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,17 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.annotation.Email;
+
 public class UserRequestDto {
+    @NotNull
+    @Email
+    @Size(max = 255)
     private String email;
+
+    @NotNull
+    @Size(min = 8, max = 255)
     private String password;
 
     public String getEmail() {
