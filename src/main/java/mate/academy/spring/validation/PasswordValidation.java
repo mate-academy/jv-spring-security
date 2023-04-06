7c8df4d2efd570c4b25b3ef1beaed3fc9ef65b1a
Olexii Melnik <alexmelnikk@gmail.com>
2023-04-06 11:54:30 +0300

    f1st commit

---
+++ src/main/java/mate/academy/spring/validation/PasswordValidation.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class PasswordValidation implements ConstraintValidator<Password, UserRequestDto> {
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{10,}$";
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        String password = userRequestDto.getPassword();
+        String repeatPassword = userRequestDto.getRepeatPassword();
+        if (!password.equals(repeatPassword)) {
+            return false;
+        }
+        return isValidPassword(password);
+    }
+
+    private boolean isValidPassword(String password) {
+        return password != null && password.matches(PASSWORD_PATTERN);
+    }
+}
