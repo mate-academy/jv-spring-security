79691fcc275fcbfab1a366bdbf3b90a55a010798
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-20 21:50:02 +0300

    configure SpringSecurity with in memory authentication

---
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("bob")
+                .password(getEncoder().encode("1234"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
