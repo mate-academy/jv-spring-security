1c0c7378a386212c0320760d223ebcbc8edddcf5
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-04-20 14:08:39 +0200

    spring-security-part1 is done

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,23 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Password;
+
+@Password
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 6)
     private String password;
+    @NotNull
+    private String repeatPassword;
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
 
     public String getEmail() {
         return email;
