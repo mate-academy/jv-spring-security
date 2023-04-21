d98b1c71061b8179ff26b5f0f1ee3d9082c74c02
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-04-21 13:31:57 +0200

    changes regarding the fields

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,18 +1,17 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
 import mate.academy.spring.validation.Password;
 
 @Password
 public class UserRequestDto {
     @Email
     private String email;
-    @NotNull
-    @Size(min = 6)
+
+    @Size(min = 6, max = 255)
     private String password;
-    @NotNull
+
     private String repeatPassword;
 
     public String getRepeatPassword() {
--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -5,13 +5,13 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String EMAIL_PATTERN =
+    private static Pattern REGEX = Pattern.compile(
                "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+" + "(?:\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)"
-                       + "*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+                       + "*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         return email != null
-                && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+                && REGEX.matcher(email).matches();
     }
 }
