d5226cddd3e533468b07eceb86e4678346bf8f11
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-21 23:10:48 +0300

    add annotations

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull
+    @Size(min = 1)
     private String title;
+    @NotNull
+    @Size(min = 2)
     private String description;
 
     public String getTitle() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @Email(message = "Not valid email")
+    @NotNull(message = "Please enter email")
+    @Size(min = 4)
     private String email;
+    @NotNull(message = "Please enter password")
+    @Size(min = 6)
     private String password;
 
     public String getEmail() {
