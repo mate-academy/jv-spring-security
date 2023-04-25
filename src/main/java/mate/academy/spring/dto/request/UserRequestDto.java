cc110314860e8dec11d0a9222a59e003fdcfa3d9
Nazar Zvarych <zvarych45@gmail.com>
2023-04-25 22:19:23 +0300

    I configure Spring security, replace all ?userId to Authentication object. I add @Valid annotations to Dto models and add CustomGlobalExceptionHandler.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,21 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @NotNull
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 8)
     private String password;
+    private String repeatPassword;
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
 
     public String getEmail() {
         return email;
