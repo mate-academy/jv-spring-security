1f2e5e6b6ca05ee9d8c70b5d05fc393f9f5e4bfc
Dima Davydenko <dmytro.davydenko89@gmail.com>
2023-04-26 17:04:07 +0300

    the solution was implemented

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @Positive
+    @Max(value = 500)
     private int capacity;
+    @NotNull
+    @Size(min = 3, max = 200)
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotEmpty
     private String title;
+    @NotEmpty
+    @Size(min = 3, max = 200)
     private String description;
 
     public String getTitle() {
