201b01d604ff5966ec64da5aecbabb5af538e56a
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-04-26 19:12:40 +0300

    jv-spring-security hw v1.1

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,21 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import mate.academy.spring.validator.Email;
+import mate.academy.spring.validator.Password;
+
+@Password
 public class UserRequestDto {
+    @Email
+    @NotNull
     private String email;
+    @NotNull
+    @Min(6)
     private String password;
+    @NotNull
+    @Min(6)
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +23,13 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
