fdf86c5d391942658bdf21b0f449b18ed5460ce0
De1eF <jatfun58@gmail.com>
2023-05-02 17:30:02 +0200

    Added missing annotations

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,7 +2,6 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import mate.academy.spring.validation.PasswordsMatch;
 
 @PasswordsMatch
