4c9c3abbdf3ebb0a2fc807a9350d0c5ed9cb3f3e
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-05-03 01:12:12 +0300

    Release

---
+++ src/main/java/mate/academy/spring/validator/EmailValidator.java
@@ -0,0 +1,15 @@
+package mate.academy.spring.validator;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final Pattern REGEX = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]"
+            + "+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return email != null && REGEX.matcher(email).matches();
+    }
+}
