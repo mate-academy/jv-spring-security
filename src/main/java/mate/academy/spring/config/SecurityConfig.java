8803d0c6b976f650349c928099a50eb5e87ecab7
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-05-02 18:43:12 +0300

    added fixes

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,23 +1,24 @@
 package mate.academy.spring.config;
 
-import java.security.SecureRandom;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private static final int BCRYPT_STRENGTH = 10;
+    private PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("olehsarapuk@gmail.com")
-                .password(getEncoder().encode("1234"))
+                .password(passwordEncoder.encode("1234"))
                 .roles("USER");
     }
 
@@ -28,10 +29,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().permitAll()
                 .and().httpBasic()
                 .and().csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder(BCRYPT_STRENGTH, new SecureRandom());
     }
 }
