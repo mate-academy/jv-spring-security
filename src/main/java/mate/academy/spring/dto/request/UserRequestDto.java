d80c4a1f7b91a1759a0e48f0f1bc8526ebf1e51d
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-05-03 15:53:50 +0300

    add @Password for password field

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -2,10 +2,12 @@ package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
 
 public class UserRequestDto {
     @Email
     private String email;
+    @Password
     @Size(min = 8)
     private String password;
     @Size(min = 8)
