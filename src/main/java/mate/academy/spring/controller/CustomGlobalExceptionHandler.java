6bd6680b1c72ffb3942eec7df81b0ea1dc5798f3
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-03 15:19:17 +0300

    Added notNull annotation, fixed parameters style, refactored email validator

--- src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/controller/CustomGlobalExceptionHandler.java
@@ -18,9 +18,9 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamps", LocalDateTime.now().toString());
         body.put("status", status.value());
--- src/main/java/mate/academy/spring/validation/EmailValidator.java
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -1,22 +1,16 @@
 package mate.academy.spring.validation;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.constraints.Email;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9]+"
+            + "(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        if (email == null) {
-            return false;
-        }
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
     }
 }
