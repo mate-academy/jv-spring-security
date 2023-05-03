1e670d0c5db86212d8b15ed791ce204b10613cfe
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-05-03 18:47:25 +0300

    minor changes

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class CinemaHallRequestDto {
-    @NotNull
     @Min(20)
     private int capacity;
     @NotNull
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,20 +1,16 @@
 package mate.academy.spring.dto.request;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Email;
 import mate.academy.spring.validation.Password;
 
 public class UserRequestDto {
-    @NotNull
     @Email
     @Size(min = 5)
     private String email;
-    @NotNull
     @Password
     @Size(min = 6)
     private String password;
-    @NotNull
     @Size(min = 6)
     private String repeatPassword;
 
