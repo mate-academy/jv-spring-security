bbe701aa429c6d12f81eb3a23bc0fb4e7b18c4a9
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-05-03 15:12:34 +0300

    remove redundancy annotation

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserRequestDto {
