731911487eb02e439c3c380c2a5cb9c8670f1d3f
Sergey gordeichuk <ya.gordeichuk@gmail.com>
2023-05-04 12:37:19 +0300

    Added validation(NotNull,Size,Email) to UserRequestDto,MovieSessionRequestDto,MovieRequestDto and CinemaHallRequestDto.

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,13 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Size(min = 20)
     private int capacity;
+    @NotNull
     private String description;
 
     public int getCapacity() {
