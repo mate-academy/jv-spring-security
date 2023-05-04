e5c0c4b9b26388d936a4fadac81af447517c7cfd
a.ponomarenko-at-201540164075 <NF1Y9pEm016SCxSUCNnwBwHKLQD+CU8N9IuhiFm/FsA=>
2023-05-04 19:08:49 +0300

    stylecheck fixed

--- src/main/java/mate/academy/spring/validator/RepeatPasswordValidator.java
+++ src/main/java/mate/academy/spring/validator/RepeatPasswordValidator.java
@@ -5,7 +5,8 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import mate.academy.spring.dto.request.UserRequestDto;
 
-public class RepeatPasswordValidator implements ConstraintValidator<PasswordRepeated, UserRequestDto> {
+public class RepeatPasswordValidator
+        implements ConstraintValidator<PasswordRepeated, UserRequestDto> {
     private static final Pattern EMAIL_PATTERN
             = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
 
