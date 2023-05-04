8f5338e9664dcd02fe3b08977131180c56049cdb
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-04 09:14:28 +0300

    Basic solution 1. InMemory authentication - added; 2. Authentication object instead ?userId @RequestParam - used; 3. Field annotations to DTOs - added; 4. Custom email validation - implemented; 5. CustomGlobalExceptionHandler - implemented; 6. login() in AuthenticationService, HashUtil and salt from User entity - removed; 7. passwordEncoder in UserService -> add() - used; 8. maven checkstyle - BUILD SUCCESS;

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
+    @Size(min = 6, max = 255)
     private String email;
+    @NotNull
+    @Size(min = 8, max = 32)
     private String password;
 
     public String getEmail() {
