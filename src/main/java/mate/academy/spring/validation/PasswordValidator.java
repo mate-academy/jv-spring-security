2b449c491f3c009dd144d346138bbeb8df86bd42
Oleksandr Koval <0993079960a@gmail.com>
2023-05-04 18:32:55 +0300

    improved solution

--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -5,9 +5,6 @@ import javax.validation.ConstraintValidatorContext;
 import mate.academy.spring.dto.request.UserRequestDto;
 
 public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
-    private static final int MIN_LENGTH = 8;
-    private static final int MAX_LENGTH = 100;
-
     @Override
     public boolean isValid(UserRequestDto userRequestDto,
                            ConstraintValidatorContext constraintValidatorContext) {
