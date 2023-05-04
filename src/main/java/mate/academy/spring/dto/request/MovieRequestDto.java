0dd02010e41b3776270badfc005f95fb59c9e799
Your Name <you@example.com>
2023-05-04 12:33:45 +0300

    add validation annotations to request dto's add Email and Password annotations and implementations for them EmailValidation and PasswordValidation add repeatPassword in UserRequestDto remove login method from AuthenticationService remove HashUtil class

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,14 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull
+    @Min(value = 4)
     private String title;
+    @Size(min = 20, max = 512)
     private String description;
 
     public String getTitle() {
