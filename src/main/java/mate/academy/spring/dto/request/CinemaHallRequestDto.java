2529dab94fe2ec5250dccc3d8a3fe60155716499
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-05-04 21:17:03 +0300

    Implemented spring-security

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Min(value = 1)
     private int capacity;
+    @NotNull
+    @NotEmpty
     private String description;
 
     public int getCapacity() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @Email
     private String email;
+    @NotNull
+    @NotEmpty
+    @Size(min = 4)
     private String password;
 
     public String getEmail() {
