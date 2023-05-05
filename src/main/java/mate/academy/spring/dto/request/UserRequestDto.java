cc05dd19d01c61acee3f0ace43e420ef7dd41483
Sergey gordeichuk <ya.gordeichuk@gmail.com>
2023-05-05 10:18:42 +0300

    Deleted @NotNull from UserRequestDto.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import mate.academy.spring.validation.Password;
 
@@ -9,10 +8,8 @@ import mate.academy.spring.validation.Password;
 public class UserRequestDto {
     @Email
     private String email;
-    @NotNull
     @Size(min = 8)
     private String password;
-    @NotNull
     @Size(min = 8)
     private String repeatPassword;
 
