bd11caf9b018d4007260dbb0001b820ea6dfb32f
Sofiia <sofiakazzz@gmail.com>
2023-05-17 17:24:51 +0300

    Was configured Spring Security, replaced @RequestParam to Authentication obj, added needs annotation, implemented custom email validator and CustomGlobalExceptionHandler.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import mate.academy.spring.validation.Email;
+import mate.academy.spring.validation.Password;
+import org.springframework.lang.NonNull;
+
+@Password
 public class UserRequestDto {
+    @Email
     private String email;
+    @NonNull
+    @Min(4)
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +20,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
