9a435f2d4a98b79a19e21dc6a854be1b56f4ae49
Sofiia <sofiakazzz@gmail.com>
2023-05-17 17:18:17 +0300

    Was configured Spring Security, replaced @RequestParam to Authentication obj, added needs annotation, implemented custom email validator and CustomGlobalExceptionHandler.

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -17,12 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cinema-halls")
 public class CinemaHallController {
     private final CinemaHallService cinemaHallService;
-    private final RequestDtoMapper<CinemaHallRequestDto, CinemaHall> cinemaHallRequestDtoMapper;
-    private final ResponseDtoMapper<CinemaHallResponseDto, CinemaHall> cinemaHallResponseDtoMapper;
+    private final RequestDtoMapper<CinemaHallRequestDto, CinemaHall>
+            cinemaHallRequestDtoMapper;
+    private final ResponseDtoMapper<CinemaHallResponseDto, CinemaHall>
+            cinemaHallResponseDtoMapper;
 
     public CinemaHallController(CinemaHallService cinemaHallService,
-            RequestDtoMapper<CinemaHallRequestDto, CinemaHall> cinemaHallRequestDtoMapper,
-            ResponseDtoMapper<CinemaHallResponseDto, CinemaHall> cinemaHallResponseDtoMapper) {
+                                RequestDtoMapper<CinemaHallRequestDto,
+                                        CinemaHall> cinemaHallRequestDtoMapper,
+                                ResponseDtoMapper<CinemaHallResponseDto, CinemaHall>
+                                        cinemaHallResponseDtoMapper) {
         this.cinemaHallService = cinemaHallService;
         this.cinemaHallRequestDtoMapper = cinemaHallRequestDtoMapper;
         this.cinemaHallResponseDtoMapper = cinemaHallResponseDtoMapper;
@@ -31,7 +35,7 @@ public class CinemaHallController {
     @PostMapping
     public CinemaHallResponseDto add(@RequestBody CinemaHallRequestDto requestDto) {
         CinemaHall cinemaHall = cinemaHallService.add(
-                    cinemaHallRequestDtoMapper.mapToModel(requestDto));
+                cinemaHallRequestDtoMapper.mapToModel(requestDto));
         return cinemaHallResponseDtoMapper.mapToDto(cinemaHall);
     }
 
