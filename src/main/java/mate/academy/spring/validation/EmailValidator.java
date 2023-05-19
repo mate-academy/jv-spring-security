9b00cd149eb535444b6a79cd36343dd7e329159e
Dmytro Koval <dimonkoval@gmail.com>
2023-05-19 15:38:21 +0300

    1

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.validation;
+
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if (email == null) {
+            return false;
+        }
+        return pattern.matcher(email).matches();
+    }
+}
