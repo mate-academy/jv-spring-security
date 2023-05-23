ee066a9b2aa989bd1c40a1abe7ea04de09fdb33c
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-23 13:56:42 +0200

    1.0 added pom.xml

---
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package mate.academy.spring.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
