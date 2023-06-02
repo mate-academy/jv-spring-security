031b87a058ebb7704c35c94126954b6bce8a3b4c
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-06-02 17:54:03 +0200

    checkstyle

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,18 +1,14 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import mate.academy.spring.validation.ValidPassword;
 
 @ValidPassword
 public class UserRequestDto {
 
     @Email
-    @NotEmpty
     private String email;
-    @NotEmpty
     private String password;
-    @NotEmpty
     private String repeatpassword;
 
     public UserRequestDto() {
