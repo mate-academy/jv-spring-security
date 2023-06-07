6aa6142b7a0d182bf550853d290a6e5d7177ea87
vova5179 <vova5179@gmail.com>
2023-06-07 16:48:52 +0300

    added implementation for jv-spring-security-hw

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,16 @@
 package mate.academy.spring.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class MovieSessionRequestDto {
+
+    @Size(min = 1)
     private Long movieId;
+    @Size(min = 1)
     private Long cinemaHallId;
+    @NotNull
     private LocalDateTime showTime;
 
     public Long getMovieId() {
