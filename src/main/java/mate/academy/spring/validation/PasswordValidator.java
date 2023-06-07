46b84d1cc2b0e0585d2c927503f0d0d5e17f6fd2
iskamele <iskamele@gmail.com>
2023-06-07 14:17:02 +0300

    Removed unnecessary code. The minimum password length has also been reduced.

--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -1,21 +1,15 @@
 package mate.academy.spring.validation;
 
-import java.util.Objects;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import mate.academy.spring.dto.request.UserRequestDto;
 
 public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
-    private static final int MIN_PASSWORD_LENGTH = 64;
+    private static final int MIN_PASSWORD_LENGTH = 8;
 
     @Override
     public boolean isValid(UserRequestDto userRequestDto,
                            ConstraintValidatorContext constraintValidatorContext) {
-        if (userRequestDto.getPassword() == null
-                || !Objects.equals(userRequestDto.getPassword(),
-                userRequestDto.getRepeatPassword())) {
-            return false;
-        }
         return userRequestDto != null
                 && userRequestDto.getPassword() != null
                 && userRequestDto.getPassword().length() >= MIN_PASSWORD_LENGTH
