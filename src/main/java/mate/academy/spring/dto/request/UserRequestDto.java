de24756935326983ba0956b4ee037d11b01f0cb8
iskamele <iskamele@gmail.com>
2023-06-07 14:16:29 +0300

    Removed unnecessary code.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.dto.request;
 
-import java.util.Objects;
 import mate.academy.spring.validation.Email;
 import mate.academy.spring.validation.Password;
 
@@ -21,19 +20,5 @@ public class UserRequestDto {
 
     public String getRepeatPassword() {
         return repeatPassword;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        UserRequestDto that = (UserRequestDto) o;
-        return Objects.equals(email, that.email)
-                && Objects.equals(password, that.password)
-                && Objects.equals(repeatPassword, that.repeatPassword);
     }
 }
