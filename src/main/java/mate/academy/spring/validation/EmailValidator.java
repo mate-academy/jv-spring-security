96664cea6facb05f83b16a8d07425f4772b34ee4
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-06-30 22:58:39 +0200

    spring security task completed

---
+++ src/main/java/mate/academy/spring/validation/EmailValidator.java
@@ -0,0 +1,13 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<Email, String> {
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+.[a-zA-Z]{2,6}$";
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email != null && email.matches(EMAIL_PATTERN);
+    }
+}
