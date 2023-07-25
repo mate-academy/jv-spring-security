34f6b3dddacb6dc2507581db65e791c2765b3aed
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-07-25 12:14:15 +0300

    jv-spring-security - second version after mentor review(added one constant).

--- src/main/java/mate/academy/spring/validator/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validator/PasswordValidator.java
@@ -6,12 +6,14 @@ import mate.academy.spring.dto.request.UserRegistrationDto;
 
 public class PasswordValidator implements ConstraintValidator<ValidPassword,
         UserRegistrationDto> {
+    private static final int MIN_LENGTH_OF_PASSWORD = 8;
+
     @Override
     public boolean isValid(UserRegistrationDto dto, ConstraintValidatorContext context) {
         String password = dto.getPassword();
         String repeatPassword = dto.getRepeatPassword();
         if (password == null || repeatPassword == null
-                || password.length() < 8 || repeatPassword.length() < 8) {
+                || password.length() < MIN_LENGTH_OF_PASSWORD) {
             return false;
         }
         return password.equals(repeatPassword);
