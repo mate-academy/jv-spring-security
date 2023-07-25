1c656c674a83c0564c355ce4391efaa021e95c5f
Chris <tynkastya@gmail.com>
2023-07-25 12:31:10 +0300

    implemented task 4

--- src/main/java/mate/academy/spring/validation/PasswordValidator.java
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -9,16 +9,16 @@ import mate.academy.spring.dto.request.UserRequestDto;
 public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
     private static final String PASSWORD_PATTERN =
             "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    private static final Pattern PATTERN = Pattern.compile(PASSWORD_PATTERN);
 
     @Override
     public boolean isValid(UserRequestDto userRequestDto,
                            ConstraintValidatorContext constraintValidatorContext) {
-        if (userRequestDto.getPassword() != null
-                && userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword())) {
-            Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-            Matcher matcher = pattern.matcher(userRequestDto.getPassword());
-            return matcher.matches();
+        if (userRequestDto.getPassword() == null
+                || !userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword())) {
+            return false;
         }
-        return false;
+        Matcher matcher = PATTERN.matcher(userRequestDto.getPassword());
+        return matcher.matches();
     }
 }
