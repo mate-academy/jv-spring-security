7c336bedc1a47f8455a17db940ccc479271b7eac
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-07-26 13:16:06 +0200

    implemented security

--- src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieSessionRequestDto.java
@@ -1,10 +1,19 @@
 package mate.academy.spring.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 public class MovieSessionRequestDto {
+    @NotNull
+    @Min(value = 1)
     private Long movieId;
+    @NotNull
+    @Min(value = 1)
     private Long cinemaHallId;
+    @NotNull
+    @Future
     private LocalDateTime showTime;
 
     public Long getMovieId() {
