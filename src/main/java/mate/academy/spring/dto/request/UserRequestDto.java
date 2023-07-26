d496d1f7d616177357d4119cc24dabc86041277b
DVArt <dvart@hotmail.com>
2023-07-26 18:48:08 +0300

    fix maven

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,10 +1,10 @@
 package mate.academy.spring.dto.request;
 
-import mate.academy.spring.validation.Password;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Password;
+
 @Password
 public class UserRequestDto {
     @Email
