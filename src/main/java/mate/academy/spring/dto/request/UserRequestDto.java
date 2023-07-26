42e0ade092ac0a371f4bebe8774e901775788146
DVArt <dvart@hotmail.com>
2023-07-26 18:46:04 +0300

    Ass annotation in UserRequestDto

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,9 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import mate.academy.spring.validation.Password;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+@Password
 public class UserRequestDto {
     @Email
     private String email;
