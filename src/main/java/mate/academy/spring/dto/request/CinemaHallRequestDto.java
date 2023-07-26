2abd391b5f92d9aef0d3f6e455835f47e28cf97c
Gamemod13 <igorfetov1997@gmail.com>
2023-07-26 12:09:01 +0300

    Fix some problem with security

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,11 +1,9 @@
 package mate.academy.spring.dto.request;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 public class CinemaHallRequestDto {
-    @NotNull
     @Positive
     private int capacity;
     @NotBlank
