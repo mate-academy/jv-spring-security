29f0a013b3b5ad8ebff7a220a8fb45bf0828d9ce
Pavlo Krasota <pavlokrasota@gmail.com>
2023-07-26 22:31:02 +0300

    fixed globalExeption

--- src/main/java/mate/academy/spring/exception/GlobalExceptionHandler.java
+++ src/main/java/mate/academy/spring/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package mate.academy.spring.exception;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
@@ -12,13 +11,11 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
@@ -26,31 +23,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatus status,
             WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
+        body.put("timestamp", LocalDateTime.now().toString());
         body.put("status", status.value());
-        List<String> errors = ex.getBindingResult()
+        body.put("errors", ex.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(this::getErrorMessage)
-                .collect(Collectors.toList());
-        body.put("errors", errors);
+                .collect(Collectors.toList()));
         return new ResponseEntity<>(body, headers, status);
-    }
-
-    @ExceptionHandler(DataProcessingException.class)
-    protected ResponseEntity<Object> dataProcessingExceptionHandler(DataProcessingException ex,
-                                                                    HttpHeaders headers) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now().toString());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
-        body.put("errors", ex.getMessage());
-        return new ResponseEntity<>(body, headers, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private String getErrorMessage(ObjectError e) {
         if (e instanceof FieldError) {
-            String field = ((FieldError) e).getField();
-            return field + " " + e.getDefaultMessage();
+            return ((FieldError) e).getField() + " " + e.getDefaultMessage();
         }
         return e.getDefaultMessage();
     }
