eee4cd8f048e6ff736082fc07294411fd6bfc62d
Victor Ruban <edmont373@gmail.com>
2023-07-27 09:50:37 +0300

    Spring security first vers.

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Size;
+import mate.academy.spring.validator.Email;
+import mate.academy.spring.validator.Password;
+
+@Password
 public class UserRequestDto {
+    @Email
+    @Size(min = 8, max = 20)
     private String email;
+    @Size(min = 8, max = 20)
     private String password;
+    @Size(min = 8, max = 20)
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +20,21 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
