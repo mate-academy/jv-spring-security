ec474e0d6972296ed81123814898edd28f2d0e32
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-07-28 22:00:20 +0300

    valid solution

---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class PasswordValidator implements ConstraintValidator<Password, UserRequestDto> {
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        if (userRequestDto.getPassword() == null
+                || userRequestDto.getRepeatPassword() == null
+                || !userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword())) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(userRequestDto.getPassword());
+        return matcher.matches();
+    }
+}
