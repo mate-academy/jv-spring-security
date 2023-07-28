3031383f650ed426a1b90ec763c27c3f675a5b5f
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-07-28 19:27:50 +0300

    custom email and password validation implemented

---
+++ src/main/java/mate/academy/spring/validation/PasswordValidator.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, UserRequestDto> {
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])"
+            + "(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$";
+
+    @Override
+    public boolean isValid(UserRequestDto dto, ConstraintValidatorContext context) {
+        if (!dto.getPassword().equals(dto.getRepeatPassword())) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(dto.getPassword());
+        return matcher.matches();
+    }
+}
