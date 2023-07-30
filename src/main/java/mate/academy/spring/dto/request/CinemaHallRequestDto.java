9556d334299cd0656f9609bda74cf9e3e31bd37a
Serhii Dudko <nechupara@gmail.com>
2023-07-30 20:18:10 +0300

    implemented first version of hw

--- src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/CinemaHallRequestDto.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CinemaHallRequestDto {
+    @NotNull
+    @Min(25)
     private int capacity;
+    @NotNull
+    @Size(min = 8, max = 250)
     private String description;
 
     public int getCapacity() {
