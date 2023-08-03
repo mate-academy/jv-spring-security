dacc83934e3047fa51012d83ccab552463de57cb
ArtemMakovskyy <photomak79@gmail.com>
2023-08-03 15:40:00 +0300

    implamantation of spring security

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,11 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+
 public class MovieRequestDto {
+    @NotEmpty(message = "Please enter title")
     private String title;
+    @NotEmpty(message = "Please enter description")
     private String description;
 
     public String getTitle() {
---
+++ src/main/java/mate/academy/spring/validation/PasswordsValidation.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import mate.academy.spring.dto.request.UserRequestDto;
+
+public class PasswordsValidation implements ConstraintValidator<Passwords, UserRequestDto> {
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        if (userRequestDto.getPassword() == null
+                || userRequestDto.getRepeatPassword() == null) {
+            return false;
+        }
+        return userRequestDto.getPassword().equals(userRequestDto.getRepeatPassword());
+    }
+}
