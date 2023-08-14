8add96a036dcba4db3e2a61226a109bca8801e6d
Ros <traine41@icloud.com>
2023-08-14 19:18:54 +0100

    modified Dto classes

--- src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/MovieRequestDto.java
@@ -1,7 +1,16 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class MovieRequestDto {
+    @NotNull
+    @NotEmpty
+    @Size(min = 5)
     private String title;
+    @NotNull
+    @Size(min = 5)
     private String description;
 
     public String getTitle() {
--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,8 +1,19 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validator.password.MatchPassword;
+
+@MatchPassword
 public class UserRequestDto {
+    @Email
+    @NotNull
     private String email;
+    @NotNull
+    @Size(min = 6)
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +21,9 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
     }
 }
