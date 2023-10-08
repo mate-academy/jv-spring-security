6ad87f0e29467aff08141f43d1c8f4c62c5ca426
ANDREY <andrey.kiyas@gmail.com>
2023-10-08 23:35:48 +0300

    hw-jv-spring-security-andrey-kiyas

--- src/main/java/mate/academy/spring/controller/MovieController.java
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -1,6 +1,8 @@
 package mate.academy.spring.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
 import mate.academy.spring.dto.request.MovieRequestDto;
 import mate.academy.spring.dto.response.MovieResponseDto;
 import mate.academy.spring.model.Movie;
@@ -21,15 +23,15 @@ public class MovieController {
     private final ResponseDtoMapper<MovieResponseDto, Movie> movieResponseDtoMapper;
 
     public MovieController(MovieService movieService,
-            RequestDtoMapper<MovieRequestDto, Movie> movieRequestDtoMapper,
-            ResponseDtoMapper<MovieResponseDto, Movie> movieResponseDtoMapper) {
+                           RequestDtoMapper<MovieRequestDto, Movie> movieRequestDtoMapper,
+                           ResponseDtoMapper<MovieResponseDto, Movie> movieResponseDtoMapper) {
         this.movieService = movieService;
         this.movieRequestDtoMapper = movieRequestDtoMapper;
         this.movieResponseDtoMapper = movieResponseDtoMapper;
     }
 
     @PostMapping
-    public MovieResponseDto add(@RequestBody MovieRequestDto requestDto) {
+    public MovieResponseDto add(@RequestBody @Valid MovieRequestDto requestDto) {
         Movie movie = movieService.add(movieRequestDtoMapper.mapToModel(requestDto));
         return movieResponseDtoMapper.mapToDto(movie);
     }
@@ -39,6 +41,6 @@ public class MovieController {
         return movieService.getAll()
                 .stream()
                 .map(movieResponseDtoMapper::mapToDto)
-                .toList();
+                .collect(Collectors.toList());
     }
 }
